23ec52c641c02a67c04880372a18c5d8eaaa590c
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2023-11-12 12:54:12 +0000

    Home manager should follow nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/master";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     darwin = {
       url = "github:LnL7/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
