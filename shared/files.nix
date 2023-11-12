47d450e4af9a12223c2e41eb89fe8827146d1a18
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2023-11-12 15:22:07 +0000

    Having serious issues with zellij.. lets see if it works this way..

--- shared/files.nix
+++ shared/files.nix
@@ -1,10 +1,10 @@
 { ... }:
 
 {
-  ".config/zellij/config.kdl" = {
-    source = ../shared/config/zellij/config.kdl;
-  };
-  ".config/zellij/layouts/default.kdl" = {
-    source = ../shared/config/zellij/layouts/default.kdl;
-  };
+  # ".config/zellij/config.kdl" = {
+  #  source = ../shared/config/zellij/config.kdl;
+  # };
+  # ".config/zellij/layouts/default.kdl" = {
+  # source = ../shared/config/zellij/layouts/default.kdl;
+  # };
 }
