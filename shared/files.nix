572878bba5119c3a81080158420fa0aa4580cc1d
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2023-11-12 13:53:32 +0000

    simplify read

--- shared/files.nix
+++ shared/files.nix
@@ -2,9 +2,9 @@
 
 {
   ".config/zellij/config.kdl" = {
-    source = builtins.readFile ../shared/config/zellij/config.kdl;
+    source = ../shared/config/zellij/config.kdl;
   };
   ".config/zellij/layouts/default.kdl" = {
-    source = builtins.readFile ../shared/config/zellij/layouts/default.kdl;
+    source = ../shared/config/zellij/layouts/default.kdl;
   };
 }
