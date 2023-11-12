31598ea3d6ff8f59f25756ecefe255e074ce345a
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2023-11-12 13:31:05 +0000

    Use source instead of text

--- shared/files.nix
+++ shared/files.nix
@@ -2,9 +2,9 @@
 
 {
   ".config/zellij/config.kdl" = {
-    text = builtins.readFile ../shared/config/zellij/config.kdl;
+    source = builtins.readFile ../shared/config/zellij/config.kdl;
   };
   ".config/zellij/layouts/default.kdl" = {
-    text = builtins.readFile ../shared/config/zellij/layouts/default.kdl;
+    source = builtins.readFile ../shared/config/zellij/layouts/default.kdl;
   };
 }
