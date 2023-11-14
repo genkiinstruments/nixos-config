0a95d87979be919d4d8ae0eae8564e1190a4c4f5
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2023-11-14 12:10:26 +0000

    Try having a declarative dock like the big guys

--- darwin/home-manager.nix
+++ darwin/home-manager.nix
@@ -65,4 +65,21 @@ in
       manual.manpages.enable = false;
     };
   };
+
+  # Fully declarative dock using the latest from Nix Store
+  local.dock.enable = true;
+  local.dock.entries = [
+    { path = "${pkgs.alacritty}/Applications/Alacritty.app/"; }
+    {
+      path = "${config.users.users.${user}.home}/.local/share/";
+      section = "others";
+      options = "--sort name --view grid --display folder";
+    }
+    {
+      path = "${config.users.users.${user}.home}/.local/share/downloads";
+      section = "others";
+      options = "--sort name --view grid --display stack";
+    }
+  ];
+
 }
