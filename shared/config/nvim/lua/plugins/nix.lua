05d18af807446758a100d9948d3b0b1f4ae632b0
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2023-11-16 16:57:42 +0000

    Try to get Nix LSP to work

--- shared/config/nvim/lua/plugins/nix.lua
+++ shared/config/nvim/lua/plugins/nix.lua
@@ -1,13 +1,4 @@
 return {
-
-    -- add svelte to treesitter
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = function(_, opts)
-            vim.list_extend(opts.ensure_installed, { "nix" })
-        end,
-    },
-
     -- correctly setup mason lsp / dap extensions
     {
         "neovim/nvim-lspconfig",
