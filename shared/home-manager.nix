87a72fd53d0f71e7e8bea463506fad338643218f
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2023-11-22 13:44:55 +0000

    Maybe this will help my precious vi mode in fish not working...https://github.com/fish-shell/fish-shell/issues/7443

--- shared/home-manager.nix
+++ shared/home-manager.nix
@@ -46,17 +46,15 @@ in
 
       # eval (zellij setup --generate-auto-start fish | string collect)
 
-      # I'm growing a neckbeard: Set the cursor shapes for the different vi modes.
-      set fish_cursor_default block blink
-      set fish_cursor_insert line blink
-      set fish_cursor_replace_one underscore blink
-      set fish_cursor_visual block
-
       function fish_user_key_bindings
-          # Execute this once per mode that emacs bindings should be used in
-          fish_default_key_bindings -M insert
-          fish_vi_key_bindings --no-erase insert
+        fish_vi_key_bindings
       end
+
+      set fish_vi_force_cursor
+      set fish_cursor_default     block      blink
+      set fish_cursor_insert      line       blink
+      set fish_cursor_replace_one underscore blink
+      set fish_cursor_visual      block
     '';
     shellInit = '' 
     set fish_greeting # Disable greeting
