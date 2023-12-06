046a05d99802ffe45a4ac12ad811c44ec31ec6b0
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2023-12-06 10:24:33 +0000

    More nvim changes

--- shared/config/nvim/lua/plugins/svelte.lua
+++ shared/config/nvim/lua/plugins/svelte.lua
@@ -16,55 +16,6 @@ return {
                 svelte = {},
                 prismals = {},
                 tailwindcss = {},
-                tsserver = {
-                    keys = {
-                        {
-                            "<leader>co",
-                            function()
-                                vim.lsp.buf.code_action({
-                                    apply = true,
-                                    context = {
-                                        only = { "source.organizeImports.ts" },
-                                        diagnostics = {},
-                                    },
-                                })
-                            end,
-                            desc = "Organize Imports",
-                        },
-                        {
-                            "<leader>cR",
-                            function()
-                                vim.lsp.buf.code_action({
-                                    apply = true,
-                                    context = {
-                                        only = { "source.removeUnused.ts" },
-                                        diagnostics = {},
-                                    },
-                                })
-                            end,
-                            desc = "Remove Unused Imports",
-                        },
-                    },
-                    settings = {
-                        typescript = {
-                            format = {
-                                indentSize = vim.o.shiftwidth,
-                                convertTabsToSpaces = vim.o.expandtab,
-                                tabSize = vim.o.tabstop,
-                            },
-                        },
-                        javascript = {
-                            format = {
-                                indentSize = vim.o.shiftwidth,
-                                convertTabsToSpaces = vim.o.expandtab,
-                                tabSize = vim.o.tabstop,
-                            },
-                        },
-                        completions = {
-                            completeFunctionCalls = true,
-                        },
-                    },
-                },
             },
         },
     },
--- shared/home-manager.nix
+++ shared/home-manager.nix
@@ -297,7 +297,7 @@ in
           indent-blankline-nvim
           clangd_extensions-nvim
           lualine-nvim
-          # neo-tree-nvim
+          neo-tree-nvim
           oil-nvim
           nvim-web-devicons
           neoconf-nvim
@@ -379,23 +379,6 @@ in
             { "nvim-treesitter/nvim-treesitter", opts = { ensure_installed = {} } },
           },
         })
-
-        local function map(mode, lhs, rhs, opts)
-            local keys = require("lazy.core.handler").handlers.keys
-            ---@cast keys LazyKeysHandler
-            -- do not create the keymap if a lazy keys handler exists
-            if not keys.active[keys.parse({ lhs, mode = mode }).id] then
-                opts = opts or {}
-                opts.silent = opts.silent ~= false
-                if opts.remap and not vim.g.vscode then
-                    opts.remap = nil
-                end
-                vim.keymap.set(mode, lhs, rhs, opts)
-            end
-        end
-
-        map({ "n", "i", "v" }, "<A-j>", "", { desc = "Move down" })
-        map({ "n", "i", "v" }, "<A-k>", "", { desc = "Move up" })
       '';
   };
 
