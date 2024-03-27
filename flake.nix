d263657262607d2764a1b724cd5ecfa17c902a79
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-03-27 14:00:58 +0000

    Minor tweak

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
         let pkgs = nixpkgs.legacyPackages.${system}; in {
           default = with pkgs; mkShell {
             nativeBuildInputs = with pkgs; [ bashInteractive git age age-plugin-yubikey ];
-            shellHook = with pkgs; ''
+            shellHook = ''
               export EDITOR=nvim
             '';
           };
