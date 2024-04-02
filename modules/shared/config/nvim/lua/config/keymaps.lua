482243ea361f024a86893d69af4621562dccfef2
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-04-02 14:25:12 +0000

    Use lua array instead

--- modules/shared/config/nvim/lua/config/keymaps.lua
+++ modules/shared/config/nvim/lua/config/keymaps.lua
@@ -1,7 +1,3 @@
 -- Unmap keymaps that move lines
-vim.keymap.del("n", "<A-j>")
-vim.keymap.del("i", "<A-j>")
-vim.keymap.del("v", "<A-j>")
-vim.keymap.del("n", "<A-k>")
-vim.keymap.del("i", "<A-k>")
-vim.keymap.del("v", "<A-k>")
+vim.keymap.del({ "n", "i", "v" }, "<A-j>")
+vim.keymap.del({ "n", "i", "v" }, "<A-k>")
