defdb681c6d74ed8f787283cd35133e22b18a74e
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-04-02 10:58:54 +0000

    Remove telekasten

--- modules/shared/config/nvim/lua/config/keymaps.lua
+++ modules/shared/config/nvim/lua/config/keymaps.lua
@@ -6,23 +6,6 @@ vim.keymap.set( { mode = "v", expr = false, noremap = true }, "<A-k>", ":m '<-2<
 vim.keymap.set( { mode = "i", expr = false, noremap = true }, "<A-j>", "<Esc>:m .+1<CR>==gi", { desc = "Move line down in insert mode" })
 vim.keymap.set( { mode = "i", expr = false, noremap = true }, "<A-k>", "<Esc>:m .-2<CR>==gi", { desc = "Move line up in insert mode" })
 
--- Launch panel if nothing is typed after <leader>z
-vim.keymap.set("n", "<leader>z", "<cmd>Telekasten panel<CR>")
-
--- Most used functions
-vim.keymap.set("n", "<leader>zf", "<cmd>Telekasten find_notes<CR>")
-vim.keymap.set("n", "<leader>zg", "<cmd>Telekasten search_notes<CR>")
-vim.keymap.set("n", "<leader>zd", "<cmd>Telekasten goto_today<CR>")
-vim.keymap.set("n", "<leader>zz", "<cmd>Telekasten follow_link<CR>")
-vim.keymap.set("n", "<leader>zn", "<cmd>Telekasten new_note<CR>")
-vim.keymap.set("n", "<leader>zc", "<cmd>Telekasten show_calendar<CR>")
-vim.keymap.set("n", "<leader>zb", "<cmd>Telekasten show_backlinks<CR>")
-vim.keymap.set("n", "<leader>zI", "<cmd>Telekasten insert_img_link<CR>")
-vim.keymap.set("n", "<leader>zt", "<cmd>Telekasten toggle_todo<CR>")
-
--- Call insert link automatically when we start typing a link
-vim.keymap.set("i", "[[", "<cmd>Telekasten insert_link<CR>")
-
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 -- stylua: ignore end
--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -316,7 +316,6 @@ in
           neodev-nvim
           noice-nvim
           nui-nvim
-          telekasten-nvim
           calendar-vim
           nvim-cmp
           nvim-lint
@@ -420,10 +419,6 @@ in
 
         -- don't show tabs
         vim.cmd [[ set showtabline=0 ]]
-
-        require('telekasten').setup({
-          home = vim.fn.expand("~/zettelkasten"), -- Put the name of your notes directory here
-        })
       '';
   };
 
