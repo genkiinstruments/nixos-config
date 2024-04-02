e29696c6c9cfdf26b85ea323338262687e5e0e01
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-04-02 10:53:55 +0000

    Update oil.lua to make sure it works properly with lazy 🚀

--- modules/shared/config/nvim/lua/plugins/oil.lua
+++ modules/shared/config/nvim/lua/plugins/oil.lua
@@ -1,6 +1,33 @@
 return {
-    "stevearc/oil.nvim",
-    opts = {},
-    -- Optional dependencies
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    {
+        "stevearc/oil.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        cmd = "Oil",
+        config = function()
+            require("oil").setup()
+        end,
+        keys = {
+            {
+                "-",
+                "<cmd>Oil<cr>",
+                { desc = "Open parent directory" },
+            },
+        },
+        opts = {
+            columns = {
+                "icon",
+                { "mtime", highlight = "Comment", format = "%T %y-%m-%d" },
+            },
+            skip_confirm_for_simple_edits = true,
+            view_options = {
+                show_hidden = true,
+            },
+            float = {
+                padding = 3,
+                win_options = {
+                    winblend = 0,
+                },
+            },
+        },
+    },
 }
