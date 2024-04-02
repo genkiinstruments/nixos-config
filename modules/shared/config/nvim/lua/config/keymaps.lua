685a2e0457cea04f7dacc9b28af22c930e9ef9ae
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-04-02 10:59:16 +0000

    oil keymap is done inside `oil.lua`

--- modules/shared/config/nvim/lua/config/keymaps.lua
+++ modules/shared/config/nvim/lua/config/keymaps.lua
@@ -5,7 +5,4 @@ vim.keymap.set( { mode = "v", expr = false, noremap = true }, "<A-j>", ":m '>+1<
 vim.keymap.set( { mode = "v", expr = false, noremap = true }, "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move block of lines up" })
 vim.keymap.set( { mode = "i", expr = false, noremap = true }, "<A-j>", "<Esc>:m .+1<CR>==gi", { desc = "Move line down in insert mode" })
 vim.keymap.set( { mode = "i", expr = false, noremap = true }, "<A-k>", "<Esc>:m .-2<CR>==gi", { desc = "Move line up in insert mode" })
-
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-
 -- stylua: ignore end
