ab2410f31880a9c43cdd97af3a9ebd25f683dccb
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-04-04 12:24:50 +0000

    More keymaps

--- modules/shared/config/nvim/lua/config/keymaps.lua
+++ modules/shared/config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,20 @@
+local keymap = vim.keymap.set
+
+keymap("n", "<C-c>", "<cmd>q<cr>", { noremap = true })
+keymap("n", "<C-x>", "<cmd>x<cr>", { noremap = true })
+
+-- floating terminal
+local lazyterm = function()
+    local Util = require("lazyvim.util")
+    Util.terminal(nil, { cwd = Util.root() })
+end
+
+keymap("n", "<c-\\>", lazyterm, { desc = "Terminal (root dir)" })
+keymap("t", "<C-\\>", "<cmd>close<cr>", { desc = "Hide Terminal" })
+
+keymap("n", "<C-/>", lazyterm, { desc = "Terminal (root dir)" })
+keymap("t", "<C-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
+
 -- Unmap keymaps that move lines
-vim.keymap.del({ "n", "i", "v" }, "<A-j>")
-vim.keymap.del({ "n", "i", "v" }, "<A-k>")
+vim.keymap.del({ "n", "i", "v", "t" }, "<A-j>")
+vim.keymap.del({ "n", "i", "v", "t" }, "<A-k>")
