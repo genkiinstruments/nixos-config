146921f60b336d36b79f7cdf40f481be56f5d1d1
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-04-04 16:20:04 +0000

    See if atuin enableFishIntegration doesn't do `atuin init fish | source`...

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -46,9 +46,6 @@ in
       }
     ];
     interactiveShellInit = /* bash */ ''
-      set -gx ATUIN_NOBIND "true"
-      atuin init fish | source
-
       # bind to ctrl-p in normal and insert mode, add any other bindings you want here too
       bind \cp _atuin_search
       bind -M insert \cp _atuin_search
