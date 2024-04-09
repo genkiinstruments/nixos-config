95c66f392cf1ff4bf665dce265df62a507371d93
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-04-09 17:04:13 +0000

    Another attempt to make harpoon play well with telescope

--- modules/shared/config/nvim/lua/plugins/harpoon.lua
+++ modules/shared/config/nvim/lua/plugins/harpoon.lua
@@ -1,11 +1,11 @@
 return {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
 		-- stylua: ignore
 		keys = {
 			{ '<leader>ua', 'ga', desc = 'Show character under cursor' },
-			{ 'ga', function() require('harpoon'):list():add() end, desc = 'Add location' },
-			{ '<header>H', function() require('harpoon'):list():add() end, desc = 'Add location' },
+			{ '<header>a', function() require('harpoon'):list():add() end, desc = 'Add location' },
 			{ '<C-n>', function() require('harpoon'):list():next() end, desc = 'Next location' },
 			{ '<C-p>', function() require('harpoon'):list():prev() end, desc = 'Previous location' },
 			{ '<leader>mr', function() require('harpoon'):list():remove() end, desc = 'Remove location' },
@@ -13,7 +13,6 @@ return {
 			{ '<leader>2', function() require('harpoon'):list():select(2) end, desc = 'Harpoon select 2' },
 			{ '<leader>3', function() require('harpoon'):list():select(3) end, desc = 'Harpoon select 3' },
 			{ '<leader>4', function() require('harpoon'):list():select(4) end, desc = 'Harpoon select 4' },
-			{ '<leader>5', function() require('harpoon'):list():select(5) end, desc = 'Harpoon select 5' },
-			{ '<leader>l', function() return require('telescope._extensions.marks')() end, desc = 'List locations' },
+			{ '<leader>5', function() require('harpoon'):list():select(5) end, desc = 'Harpoon select 5' }
   },
 }
