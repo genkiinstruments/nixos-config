cd0812807e6768fdef977de79987b5bd432225d1
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-04-09 16:50:07 +0000

    Hmm didn't work either, let's try this instead :-)

--- modules/shared/config/nvim/lua/plugins/harpoon.lua
+++ modules/shared/config/nvim/lua/plugins/harpoon.lua
@@ -1,39 +1,18 @@
 return {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
-    opts = {
-        menu = {
-            width = vim.api.nvim_win_get_width(0) - 4,
-        },
-    },
-    keys = function()
-        local keys = {
-            {
-                "<leader>H",
-                function()
-                    require("harpoon"):list():add()
-                end,
-                desc = "Harpoon File",
-            },
-            {
-                "<leader>h",
-                function()
-                    local harpoon = require("harpoon")
-                    harpoon.ui:toggle_quick_menu(harpoon:list())
-                end,
-                desc = "Harpoon Quick Menu",
-            },
-        }
-
-        for i = 1, 5 do
-            table.insert(keys, {
-                "<leader>" .. i,
-                function()
-                    require("harpoon"):list():select(i)
-                end,
-                desc = "Harpoon to File " .. i,
-            })
-        end
-        return keys
-    end,
+		-- stylua: ignore
+		keys = {
+			{ '<leader>ua', 'ga', desc = 'Show character under cursor' },
+			{ 'ga', function() require('harpoon'):list():append() end, desc = 'Add location' },
+			{ '<C-n>', function() require('harpoon'):list():next() end, desc = 'Next location' },
+			{ '<C-p>', function() require('harpoon'):list():prev() end, desc = 'Previous location' },
+			{ '<leader>mr', function() require('harpoon'):list():remove() end, desc = 'Remove location' },
+			{ '<leader>1', function() require('harpoon'):list():select(1) end, desc = 'Harpoon select 1' },
+			{ '<leader>2', function() require('harpoon'):list():select(2) end, desc = 'Harpoon select 2' },
+			{ '<leader>3', function() require('harpoon'):list():select(3) end, desc = 'Harpoon select 3' },
+			{ '<leader>4', function() require('harpoon'):list():select(4) end, desc = 'Harpoon select 4' },
+			{ '<leader>5', function() require('harpoon'):list():select(5) end, desc = 'Harpoon select 5' },
+			{ '<leader>l', function() return require('telescope._extensions.marks')() end, desc = 'List locations' },
+  },
 }
