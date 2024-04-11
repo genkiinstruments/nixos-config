3454de8efa06992035048d18cc928d895dcfc935
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-04-11 15:52:29 +0000

    Remove `isHidden` value from home-manager.nix for nixos

--- modules/gdrn/home-manager.nix
+++ modules/gdrn/home-manager.nix
@@ -1,18 +1,16 @@
-{ config, pkgs, user, ... }:
+{ pkgs, user, ... }:
 
 {
-  imports = [ 
-  ../shared/home-manager.nix 
+  imports = [
+    ../shared/home-manager.nix
   ];
 
   users.users.${user} = {
     name = "${user}";
-    home = "/Users/${user}";
-    isHidden = false;
     shell = pkgs.fish;
-    openssh.authorizedKeys.keys = [ 
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGqWL96+z6Wk2IgF6XRyoZAVUXmCmP8I78dUpA4Qy4bh genki@gdrn" 
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ1uxevLNJOPIPRMh9G9fFSqLtYjK5R7+nRdtsas2KwX"
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGqWL96+z6Wk2IgF6XRyoZAVUXmCmP8I78dUpA4Qy4bh genki@gdrn"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ1uxevLNJOPIPRMh9G9fFSqLtYjK5R7+nRdtsas2KwX"
     ];
   };
 
