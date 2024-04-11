e6b030d3047e15210fe53376152f3124f7403753
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-04-11 20:39:18 +0000

    Better harpoon keybindings

--- modules/shared/config/nvim/lua/plugins/harpoon.lua
+++ modules/shared/config/nvim/lua/plugins/harpoon.lua
@@ -23,6 +23,7 @@ return {
 		-- stylua: ignore
 		keys = {
 			{ '<leader>a', function() require('harpoon'):list():add() end, desc = 'Add harpoon location' },
+			{ '<leader>H', function() require('harpoon'):list():add() end, desc = 'Add harpoon location' },
 			{ '<C-n>', function() require('harpoon'):list():next() end, desc = 'Next harpoon location' },
 			{ '<C-p>', function() require('harpoon'):list():prev() end, desc = 'Previous harpoon location' },
 			{ '<leader>mr', function() require('harpoon'):list():remove() end, desc = 'Remove harpoon location' },
@@ -32,6 +33,6 @@ return {
 			{ '<leader>4', function() require('harpoon'):list():select(4) end, desc = 'Harpoon select 4' },
 			{ '<leader>5', function() require('harpoon'):list():select(5) end, desc = 'Harpoon select 5' },
       { "<C-e>", function() toggle_telescope(require("harpoon"):list()) end, desc = "Open harpoon window"},
-      { "<leader>l", function() toggle_telescope(require("harpoon"):list()) end, desc = "Open harpoon window"}
+      { "<leader>h", function() toggle_telescope(require("harpoon"):list()) end, desc = "Open harpoon window"}
   },
 }
