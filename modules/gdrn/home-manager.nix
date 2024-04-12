4b6172b6c3140d73bbc9b0087782c462cd3d620d
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-04-12 15:54:48 +0000

    Simplify home-manager stuff

--- modules/gdrn/home-manager.nix
+++ modules/gdrn/home-manager.nix
@@ -1,29 +1,6 @@
-{ pkgs, user, ... }:
-
+_:
 {
   imports = [
     ../shared/home-manager.nix
   ];
-
-  users.users.${user} = {
-    name = "${user}";
-    shell = pkgs.fish;
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGqWL96+z6Wk2IgF6XRyoZAVUXmCmP8I78dUpA4Qy4bh genki@gdrn"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ1uxevLNJOPIPRMh9G9fFSqLtYjK5R7+nRdtsas2KwX"
-    ];
-  };
-
-  # Since we're using fish as our shell
-  programs.fish.enable = true;
-
-  home-manager = {
-    useGlobalPkgs = true;
-    users.${user} = { ... }:
-      {
-        home.enableNixpkgsReleaseCheck = false;
-        home.stateVersion = "23.05";
-        xdg.enable = true; # Needed for fish interactiveShellInit hack
-      };
-  };
 }
