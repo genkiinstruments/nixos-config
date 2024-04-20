73603a70e6f0254d5510f0a752653ba523851cca
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-04-20 17:54:12 +0000

    Add cachix binary cache

--- modules/shared/cachix/default.nix
+++ modules/shared/cachix/default.nix
@@ -2,11 +2,14 @@ _:
 {
   nix.settings = {
     substituters = [
+      "https://genki.cachix.org"
       "https://nix-community.cachix.org"
-      "https://cache.nixos.org/"
+      "https://cache.nixos.org"
     ];
     trusted-public-keys = [
+      "genki.cachix.org-1:5l+wAa4rDwhcd5Wm43eK4N73qJ6GIKmJQ87Nw/bRGfE="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     ];
   };
 }
