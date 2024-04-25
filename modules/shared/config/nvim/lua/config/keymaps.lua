9d12e71e096ae1e3c6775ca681c1f738d07f8a58
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-04-25 23:37:13 +0000

    better nvim keymaps. map zz to ZZ

--- modules/shared/config/nvim/lua/config/keymaps.lua
+++ modules/shared/config/nvim/lua/config/keymaps.lua
@@ -2,25 +2,12 @@ local keymap = vim.keymap.set
 
 keymap("n", "<C-c>", "<cmd>q<cr>", { noremap = true })
 keymap("n", "<C-x>", "<cmd>x<cr>", { noremap = true })
-
--- floating terminal
-local lazyterm = function()
-    local Util = require("lazyvim.util")
-    Util.terminal(nil, { cwd = Util.root() })
-end
-
-keymap("n", "<c-\\>", lazyterm, { desc = "Terminal (root dir)" })
-keymap("t", "<C-\\>", "<cmd>close<cr>", { desc = "Hide Terminal" })
-
-keymap("n", "<C-/>", lazyterm, { desc = "Terminal (root dir)" })
-keymap("t", "<C-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
-
-keymap("n", "<C-þ>", lazyterm, { desc = "Terminal (root dir)" })
-keymap("t", "<C-þ>", "<cmd>close<cr>", { desc = "Hide Terminal" })
+keymap("n", "zz", "ZZ", { noremap = true })
 
 -- Unmap keymaps that move lines
-vim.keymap.del({ "n", "i", "v" }, "<A-j>")
-vim.keymap.del({ "n", "i", "v" }, "<A-k>")
+for _, val in pairs({ "<A-j>", "<A-k>" }) do
+    vim.keymap.del({ "n", "i", "v" }, val)
+end
 
 -- Don't want navigation in terminal-mode
 for _, val in pairs({ "<C-h>", "<C-j>", "<C-k>", "<C-l>" }) do
