821214a47e65f2a1dfb26ac5906cab6c1431d988
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-05-12 23:17:17 +0000

    Add elixir lsp, lint, format...

--- modules/shared/config/nvim/lua/plugins/treesitter.lua
+++ modules/shared/config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,9 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
-        vim.list_extend(opts.ensure_installed, { "regex", "bash", "markdown", "markdown_inline" })
+        vim.list_extend(
+            opts.ensure_installed,
+            { "regex", "bash", "markdown", "markdown_inline", "elixir", "heex", "eex" }
+        )
     end,
 }
