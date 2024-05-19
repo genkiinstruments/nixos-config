d9df8e0652da7d336eba9cf8ce72ef2e77ce45fc
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-05-19 19:13:54 +0000

    Not using

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -465,14 +465,6 @@
                 },
               })
 
-              -- Note: This is a workaround due to a bug I don't know where is, either in mini-comment of the kdl treesitter spec?
-              vim.api.nvim_create_autocmd("FileType", {
-                pattern = "kdl",
-                callback = function()
-                  vim.bo.commentstring = "//%s"
-                end
-              })
-
               -- Disable swap files
               vim.opt.swapfile = false
 
