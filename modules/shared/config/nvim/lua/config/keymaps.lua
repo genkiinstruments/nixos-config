07f47dda3696d5eb5d26a3025e94b034631205bc
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-05-19 12:46:02 +0000

    better keymaps for quickfix/trouble

--- modules/shared/config/nvim/lua/config/keymaps.lua
+++ modules/shared/config/nvim/lua/config/keymaps.lua
@@ -5,10 +5,15 @@ keymap("n", "<C-x>", "<cmd>x<cr>", { noremap = true })
 
 -- Unmap keymaps that move lines
 for _, val in pairs({ "<A-j>", "<A-k>" }) do
-	vim.keymap.del({ "n", "i", "v" }, val)
+    vim.keymap.del({ "n", "i", "v" }, val)
 end
 
 -- Don't want navigation in terminal-mode
 for _, val in pairs({ "<C-h>", "<C-j>", "<C-k>", "<C-l>" }) do
-	vim.keymap.del("t", val)
+    vim.keymap.del("t", val)
 end
+
+-- Primagen inspired keymaps for trouble/quickfix
+keymap("n", "<c-k>", vim.cmd.cprev, { desc = "Previous Quickfix" })
+keymap("n", "<c-j>", vim.cmd.cnext, { desc = "Next Quickfix" })
+
