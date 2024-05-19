6bb69219f76ceb620141020fe2275af75c7fba1f
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-05-19 12:35:50 +0000

    Zellij floating pane not showing up on ctrl-/.. need to investigate further

--- modules/shared/config/nvim/lua/config/keymaps.lua
+++ modules/shared/config/nvim/lua/config/keymaps.lua
@@ -5,16 +5,10 @@ keymap("n", "<C-x>", "<cmd>x<cr>", { noremap = true })
 
 -- Unmap keymaps that move lines
 for _, val in pairs({ "<A-j>", "<A-k>" }) do
-    vim.keymap.del({ "n", "i", "v" }, val)
+	vim.keymap.del({ "n", "i", "v" }, val)
 end
 
 -- Don't want navigation in terminal-mode
 for _, val in pairs({ "<C-h>", "<C-j>", "<C-k>", "<C-l>" }) do
-    vim.keymap.del("t", val)
+	vim.keymap.del("t", val)
 end
-
--- Disable lazyterm
-vim.keymap.del("n", "<leader>ft")
-vim.keymap.del("n", "<leader>fT")
-vim.keymap.del("n", "<c-/>")
-vim.keymap.del("n", "<c-_>")
