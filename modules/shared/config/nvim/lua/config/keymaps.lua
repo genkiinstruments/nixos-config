487f459c508249caf5a3fdf66b30e386fed822ce
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-05-19 20:10:38 +0000

    better quickfix/trouble navigation keymap

--- modules/shared/config/nvim/lua/config/keymaps.lua
+++ modules/shared/config/nvim/lua/config/keymaps.lua
@@ -13,8 +13,27 @@ for _, val in pairs({ "<C-h>", "<C-j>", "<C-k>", "<C-l>" }) do
     vim.keymap.del("t", val)
 end
 
--- Primagen inspired keymaps for trouble/quickfix
-keymap("n", "<c-k>", vim.cmd.cprev, { desc = "Previous Quickfix" })
-keymap("n", "<c-j>", vim.cmd.cnext, { desc = "Next Quickfix" })
-
 keymap("i", "jj", "<Esc>", { desc = "Exit insert mode" })
+
+keymap("n", "<c-k>", function()
+    if require("trouble").is_open() then
+        require("trouble").previous({ skip_groups = true, jump = true })
+    else
+        local ok, err = pcall(vim.cmd.cprev)
+        if not ok then
+            vim.notify(err, vim.log.levels.ERROR)
+        end
+    end
+end, { desc = "Previous Trouble/Quickfix Item" })
+
+keymap("n", "<c-j>", function()
+    if require("trouble").is_open() then
+        require("trouble").next({ skip_groups = true, jump = true })
+    else
+        local ok, err = pcall(vim.cmd.cnext)
+        if not ok then
+            ---@diagnostic disable-next-line: param-type-mismatch
+            vim.notify(err, vim.log.levels.ERROR)
+        end
+    end
+end, { desc = "Next Trouble/Quickfix Item" })
