1ccfb1257f6e7311bcba62f350b29341672a670e
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-06-18 14:42:44 +0000

    Use tmux, tmux-navigator and sesh... it's great!

--- modules/shared/config/nvim/lua/config/keymaps.lua
+++ modules/shared/config/nvim/lua/config/keymaps.lua
@@ -14,26 +14,3 @@ for _, val in pairs({ "<C-h>", "<C-j>", "<C-k>", "<C-l>" }) do
 end
 
 keymap("i", "jj", "<Esc>", { desc = "Exit insert mode" })
-
-keymap("n", "<c-k>", function()
-    if require("trouble").is_open() then
-        require("trouble").previous({ skip_groups = true, jump = true })
-    else
-        local ok, err = pcall(vim.cmd.cprev)
-        if not ok then
-            vim.notify(err, vim.log.levels.ERROR)
-        end
-    end
-end, { desc = "Previous Trouble/Quickfix Item" })
-
-keymap("n", "<c-j>", function()
-    if require("trouble").is_open() then
-        require("trouble").next({ skip_groups = true, jump = true })
-    else
-        local ok, err = pcall(vim.cmd.cnext)
-        if not ok then
-            ---@diagnostic disable-next-line: param-type-mismatch
-            vim.notify(err, vim.log.levels.ERROR)
-        end
-    end
-end, { desc = "Next Trouble/Quickfix Item" })
