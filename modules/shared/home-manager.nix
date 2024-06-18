df427572d15ccd6b527e55b850b899b22f884bb1
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-06-18 14:43:49 +0000

    treesitter ensure enabled
    https://github.com/LazyVim/LazyVim/discussions/1972#discussioncomment-9804886

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -462,7 +462,11 @@
                   -- import/override with your plugins
                   { import = "plugins" },
                   -- treesitter handled by xdg.configFile."nvim/parser", put this line at the end of spec to clear ensure_installed
-                  { "nvim-treesitter/nvim-treesitter", opts = { ensure_installed = {} } },
+                  { "nvim-treesitter/nvim-treesitter",
+                    opts = function(_, opts)
+                      opts.ensure_installed = {}
+                    end,
+                  },
                 },
               })
 
