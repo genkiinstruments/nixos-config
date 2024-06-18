8084256f17c903ea392f91368e05dc6e24b337b2
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-06-18 14:44:45 +0000

    Declarative dock build is broken in nixpkgs... don't really use it at all

--- modules/darwin/home-manager.nix
+++ modules/darwin/home-manager.nix
@@ -1,12 +1,10 @@
-{ config, pkgs, user, ... }:
+{ user, ... }:
 
 {
   imports = [
-    ../darwin/dock
     ../shared/home-manager.nix
   ];
 
-  # nix-darwin specific configuration, e.g., we don't want to hide the user
   users.users.${user} = {
     isHidden = false;
     home = "/Users/${user}";
@@ -27,20 +25,4 @@
     {
       home.file.".config/karabiner/karabiner.json".source = ../darwin/config/karabiner/karabiner.json; # Hyper-key config
     };
-
-  # Fully declarative dock using the latest from Nix Store
-  local.dock.enable = true;
-  local.dock.entries = [
-    { path = "${pkgs.alacritty}/Applications/Alacritty.app/"; }
-    {
-      path = "${config.users.users.${user}.home}/.local/share/";
-      section = "others";
-      options = "--sort name --view grid --display folder";
-    }
-    {
-      path = "${config.users.users.${user}.home}/.local/share/downloads";
-      section = "others";
-      options = "--sort name --view grid --display stack";
-    }
-  ];
 }
