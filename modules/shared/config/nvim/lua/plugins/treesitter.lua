5228d8c54b5e923fbc8fa19979b9e6f2248f1caa
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-07-05 13:51:45 +0000

    syntax

--- modules/shared/config/nvim/lua/plugins/treesitter.lua
+++ modules/shared/config/nvim/lua/plugins/treesitter.lua
@@ -1,9 +1,6 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
-        vim.list_extend(
-            opts.ensure_installed,
-            { "regex", "bash", "markdown", "markdown_inline", "elixir", "heex", "eex" }
-        )
+        vim.list_extend(opts.ensure_installed, { "regex", "bash", "elixir", "heex", "eex" })
     end,
 }
