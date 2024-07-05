ab527798635b87812126d0b9923c6fef77d98a52
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-07-05 17:01:11 +0000

    fish /run/current-system

--- modules/nix-deployment/home-manager.nix
+++ modules/nix-deployment/home-manager.nix
@@ -6,17 +6,13 @@
   ];
 
   users.users.${user} = {
-    name = "${user}";
-    shell = pkgs.fish;
+    shell = "/run/current-system/sw/bin/fish";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGqWL96+z6Wk2IgF6XRyoZAVUXmCmP8I78dUpA4Qy4bh genki@gdrn"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ1uxevLNJOPIPRMh9G9fFSqLtYjK5R7+nRdtsas2KwX"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJbdC7LjlCTSRadDqz5UIeCBsvekpoN2vMXUrl8R58Vf daniel@genkiinstruments.com"
     ];
   };
-
-  # Since we're using fish as our shell
-  programs.fish.enable = true;
 
   home-manager = {
     useGlobalPkgs = true;
--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -1,8 +1,7 @@
 { pkgs, pkgs-stable, lib, inputs, name, user, email, ... }:
 {
   users.users.${user} = {
-    name = "${user}";
-    shell = pkgs.fish;
+    shell = "/run/current-system/sw/bin/fish";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGqWL96+z6Wk2IgF6XRyoZAVUXmCmP8I78dUpA4Qy4bh genki@gdrn"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ1uxevLNJOPIPRMh9G9fFSqLtYjK5R7+nRdtsas2KwX olafur@M3.localdomain"
@@ -10,9 +9,6 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJbdC7LjlCTSRadDqz5UIeCBsvekpoN2vMXUrl8R58Vf daniel@genkiinstruments.com"
     ];
   };
-
-  # Since we're using fish as our shell
-  programs.fish.enable = true;
 
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
