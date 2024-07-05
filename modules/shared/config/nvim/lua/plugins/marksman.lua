a3c880dda03b36546287a1ffb799e7dec8ecec06
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-07-05 13:51:54 +0000

    markdown-nvim

---
+++ modules/shared/config/nvim/lua/plugins/marksman.lua
@@ -0,0 +1,16 @@
+-- return {
+--     "neovim/nvim-lspconfig",
+--     opts = {
+--         servers = {
+--             marksman = {},
+--         },
+--     },
+-- }
+--
+return {
+    "tadmccorkle/markdown.nvim",
+    ft = "markdown", -- or 'event = "VeryLazy"'
+    opts = {
+        -- configuration here or empty for defaults
+    },
+}
