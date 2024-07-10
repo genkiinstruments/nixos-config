5db5446c960970b643745a44ae58b8c2a5f29415
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-07-10 17:08:35 +0000

    nix.gc is different between nix-darwin and nixos

--- modules/shared/default.nix
+++ modules/shared/default.nix
@@ -27,12 +27,6 @@
         flake-registry = ${empty_registry}
         builders-use-substitutes = true
       '';
-    gc = {
-      user = "root";
-      automatic = true;
-      interval = { Weekday = 0; Hour = 2; Minute = 0; };
-      options = "--delete-older-than 30d";
-    };
 
     settings = {
       trusted-users = [ "root" "@wheel" ];
