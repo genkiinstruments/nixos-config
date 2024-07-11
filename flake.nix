1fa713bd9ea77eb11ce367bf63bed0148850eb0a
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-07-11 16:35:46 +0000

    need systemd stuff too for atuind

--- flake.nix
+++ flake.nix
@@ -255,6 +255,18 @@
                   programs.git = { inherit userEmail userName; };
                   programs.atuin.settings.daemon.enabled = true; # https://github.com/atuinsh/atuin/issues/952#issuecomment-2199964530
                 };
+              systemd.user.services.atuind = { pkgs, ... }: {
+                enable = true;
+
+                environment = {
+                  ATUIN_LOG = "info";
+                };
+                serviceConfig = {
+                  ExecStart = "${pkgs.atuin}/bin/atuin daemon";
+                };
+                after = [ "network.target" ];
+                wantedBy = [ "default.target" ];
+              };
               programs.fish.enable = true; # Otherwise our shell won't be installed correctly
               services.tailscale.enable = true;
               system.stateVersion = "23.05";
