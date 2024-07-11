73f924aeb47a03f6aaad3b2baa40f3ad1869e2cb
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-07-11 16:38:11 +0000

    Move into separate file

---
+++ hosts/biggimaus/default.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }: {
+  systemd.user.services.atuind = {
+    enable = true;
+
+    environment = {
+      ATUIN_LOG = "info";
+    };
+    serviceConfig = {
+      ExecStart = "${pkgs.atuin}/bin/atuin daemon";
+    };
+    after = [ "network.target" ];
+    wantedBy = [ "default.target" ];
+  };
+}
