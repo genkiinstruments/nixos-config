abcefe342fe1c5cdf1627726e918acc5a098ad18
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-07-29 17:24:55 +0000

    add gugusar

--- flake.nix
+++ flake.nix
@@ -215,6 +215,94 @@
               programs.ssh.startAgent = true;
             }];
           };
+        gugusar =
+          let
+            user = "genki";
+            userName = "Ólafur Bjarki Bogason";
+            userEmail = "olafur@genkiinstruments.com";
+          in
+          nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [{
+              imports = [
+                home-manager.nixosModules.home-manager
+                disko.nixosModules.disko
+                ./modules/shared
+              ];
+              disko.devices = {
+                disk = {
+                  main = {
+                    device = "/dev/disk/by-id/ata-TOSHIBA_KSG60ZMV256G_M.2_2280_256GB_583B83NWK5S";
+                    type = "disk";
+                    content = {
+                      type = "gpt";
+                      partitions = {
+                        boot = {
+                          size = "1M";
+                          type = "EF02"; # for grub MBR
+                        };
+                        ESP = {
+                          size = "1G";
+                          type = "EF00";
+                          content = {
+                            type = "filesystem";
+                            format = "vfat";
+                            mountpoint = "/boot";
+                          };
+                        };
+                        root = {
+                          size = "100%";
+                          content = {
+                            type = "filesystem";
+                            format = "ext4";
+                            mountpoint = "/";
+                          };
+                        };
+                      };
+                    };
+                  };
+                };
+              };
+              boot = {
+                loader.systemd-boot.enable = true;
+                loader.efi.canTouchEfiVariables = true;
+                initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
+                kernelModules = [ "kvm-intel" ];
+              };
+              networking.hostName = "gugusar";
+              networking.useDHCP = true;
+              users.users.${user} = {
+                isNormalUser = true;
+                shell = "/run/current-system/sw/bin/fish";
+                openssh.authorizedKeys.keyFiles = [ ./authorized_keys ];
+                extraGroups = [ "wheel" ];
+              };
+              users.users.root.openssh.authorizedKeys.keyFiles = [ ./authorized_keys ];
+              security.sudo.execWheelOnly = true;
+              security.sudo.extraConfig = ''Defaults lecture = never'';
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "backup";
+              home-manager.users.${user} = { config, ... }:
+                {
+                  imports = [
+                    nix-index-database.hmModules.nix-index
+                    catppuccin.homeManagerModules.catppuccin
+                    ./modules/shared/home.nix
+                  ];
+                  catppuccin = {
+                    enable = true;
+                    flavor = "mocha";
+                  };
+                  programs.git = { inherit userEmail userName; };
+                };
+              programs.fish.enable = true; # Otherwise our shell won't be installed correctly
+              services.tailscale.enable = true;
+              services.openssh.enable = true;
+              programs.ssh.startAgent = true;
+              system.stateVersion = "23.05";
+            }];
+          };
         kroli =
           let
             user = "genki";
@@ -275,10 +363,11 @@
                 isNormalUser = true;
                 shell = "/run/current-system/sw/bin/fish";
                 openssh.authorizedKeys.keyFiles = [ ./authorized_keys ];
-                hashedPassword = "";
                 extraGroups = [ "wheel" ];
               };
               users.users.root.openssh.authorizedKeys.keyFiles = [ ./authorized_keys ];
+              security.sudo.execWheelOnly = true;
+              security.sudo.extraConfig = ''Defaults lecture = never'';
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.backupFileExtension = "backup";
