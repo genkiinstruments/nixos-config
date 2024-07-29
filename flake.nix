96657ab8015a121991885584d4f8ca254a9cf623
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-07-29 13:27:55 +0000

    disko-config

--- flake.nix
+++ flake.nix
@@ -227,7 +227,40 @@
                 ./modules/shared
                 ./hosts/biggimaus/disk-config.nix
               ];
-              disko.devices.disk.main.device = "/dev/disk/by-id/ata-SanDisk_SD8SN8U512G1002_175124804870";
+              disko.devices = {
+                disk = {
+                  main = {
+                    device = "/dev/disk/by-id/ata-SanDisk_SD8SN8U512G1002_175124804870";
+                    type = "disk";
+                    content = {
+                      type = "gpt";
+                      partitions = {
+                        boot = {
+                          size = "1M";
+                          type = "EF02"; # for grub MBR
+                        };
+                        ESP = {
+                          size = "1G";
+                          type = "EF00";
+                          content = {
+                            type = "filesystem";
+                            format = "vfat";
+                            mountpoint = "/boot";
+                          };
+                        };
+                        root = {
+                          size = "100%";
+                          content = {
+                            type = "filesystem";
+                            format = "ext4";
+                            mountpoint = "/";
+                          };
+                        };
+                      };
+                    };
+                  };
+                };
+              };
               boot = {
                 loader.systemd-boot.enable = true;
                 loader.efi.canTouchEfiVariables = true;
