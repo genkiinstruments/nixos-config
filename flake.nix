7ae3440b5bb004e17bd067413f7ff8ab8af96a75
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-08-01 22:27:03 +0000

    missing letter in kroli's main disk

--- flake.nix
+++ flake.nix
@@ -303,7 +303,7 @@
               disko.devices = {
                 disk = {
                   main = {
-                    device = "/dev/disk/by-id/ata-TOSHIBA_KSG60ZMV256G_M.2_2280_256GB_583B83NWK5S";
+                    device = "/dev/disk/by-id/ata-TOSHIBA_KSG60ZMV256G_M.2_2280_256GB_583B83NWK5SP";
                     type = "disk";
                     content = {
                       type = "gpt";
