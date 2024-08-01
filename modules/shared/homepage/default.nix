9be2420279476db87e7fd18376129445b6fdcc42
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-08-01 12:50:06 +0000

    joip

--- modules/shared/homepage/default.nix
+++ modules/shared/homepage/default.nix
@@ -1,15 +1,13 @@
-{
-  hostname,
-  lib,
-  pkgs,
-  inputs,
-  ...
+{ pkgs
+, inputs
+, ...
 }:
 {
   disabledModules = [ "services/misc/homepage-dashboard.nix" ];
   imports = [
     "${inputs.unstable}/nixos/modules/services/misc/homepage-dashboard.nix"
-  ] ++ lib.optional (builtins.pathExists (./. + "/${hostname}.nix")) ./${hostname}.nix;
+    ./joip.nix
+  ];
 
   services.homepage-dashboard = {
     enable = true;
