c7e24208d76af4daab80566c837bd7891f572325
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-08-07 15:37:48 +0000

    update homepage-dashboard

--- modules/shared/homepage/default.nix
+++ modules/shared/homepage/default.nix
@@ -1,9 +1,26 @@
-_:
-{
+_: {
   imports = [ ./joip.nix ];
 
-  services.homepage-dashboard = {
-    enable = true;
-    openFirewall = true;
+  services = {
+    homepage-dashboard = {
+      enable = true;
+      openFirewall = true;
+    };
+
+    caddy = {
+      enable = true;
+      virtualHosts."joip.tail01dbd.ts.net".extraConfig = ''
+        reverse_proxy http://localhost:8082
+      '';
+    };
+
+    tailscale = {
+      enable = true;
+      openFirewall = true;
+      useRoutingFeatures = "both";
+      permitCertUid = "caddy";
+    };
+
+    resolved.enable = true;
   };
 }
