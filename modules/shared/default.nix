b3d85399e795ef677fe2b9b00422a2a38b2c5889
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-08-14 15:44:51 +0000

    fmt

--- modules/shared/default.nix
+++ modules/shared/default.nix
@@ -10,12 +10,15 @@
 
     # Apply each overlay found in the /overlays directory
     overlays =
-      let path = ../../overlays; in with builtins;
-      map (n: import (path + ("/" + n)))
-        (filter
-          (n: match ".*\\.nix" n != null ||
-            pathExists (path + ("/" + n + "/default.nix")))
-          (attrNames (readDir path)));
+      let
+        path = ../../overlays;
+      in
+      with builtins;
+      map (n: import (path + ("/" + n))) (
+        filter (n: match ".*\\.nix" n != null || pathExists (path + ("/" + n + "/default.nix"))) (
+          attrNames (readDir path)
+        )
+      );
   };
 
   nix = {
