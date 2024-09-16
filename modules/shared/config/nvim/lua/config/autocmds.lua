6bae24a7ddac72108e73296f84f87a7940cfb95b
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-09-16 16:07:07 +0000

    markdown was super slow because of keymap using "i" aka insert mode...

--- modules/shared/config/nvim/lua/config/autocmds.lua
+++ modules/shared/config/nvim/lua/config/autocmds.lua
@@ -2,16 +2,13 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-local function augroup(name)
-    return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
-end
-
--- Disable aut spell-check set by lazyvim
+-- Disable aut spell-check and ncmp for text files set by lazyvimk
 vim.api.nvim_create_autocmd("FileType", {
-    group = augroup("wrap_spell"),
+    group = vim.api.nvim_create_augroup("lazyvim_" .. "wrap_spell", { clear = true }),
     pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
     callback = function()
         vim.opt_local.wrap = true
         vim.opt_local.spell = false
+        require("cmp").setup.buffer({ enabled = false })
     end,
 })
