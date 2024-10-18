6626a97dc4c3cb163dbe561919e68e7c377e646c
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-10-18 19:11:02 +0000

    biggimaus

--- flake.nix
+++ flake.nix
@@ -142,78 +142,6 @@
           inherit specialArgs;
           modules = [ ./hosts/kroli ];
         };
-        biggimaus =
-          let
-            user = "genki";
-            userName = "Ólafur Bjarki Bogason";
-            userEmail = "olafur@genkiinstruments.com";
-          in
-          nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-              {
-                imports = [
-                  srvos.nixosModules.server
-                  srvos.nixosModules.mixins-systemd-boot
-                  srvos.nixosModules.mixins-terminfo
-                  srvos.nixosModules.mixins-nix-experimental
-                  srvos.nixosModules.mixins-trusted-nix-caches
-                  disko.nixosModules.disko
-                  home-manager.nixosModules.home-manager
-                  ./modules/shared
-                  ./hosts/biggimaus/disko-config.nix
-                ];
-                disko.devices.disk.main.device = "/dev/disk/by-id/nvme-eui.002538db21a8a97f";
-                boot = {
-                  loader.systemd-boot.enable = true;
-                  loader.efi.canTouchEfiVariables = true;
-                  binfmt.emulatedSystems = [ "aarch64-linux" ];
-                  initrd.availableKernelModules = [
-                    "xhci_pci"
-                    "ahci"
-                    "nvme"
-                    "usbhid"
-                    "usb_storage"
-                    "sd_mod"
-                  ];
-                  kernelModules = [ "kvm-intel" ];
-                };
-                networking.hostName = "biggimaus";
-                networking.hostId = "deadbeef";
-                networking.useDHCP = true;
-                users.users.${user} = {
-                  isNormalUser = true;
-                  shell = "/run/current-system/sw/bin/fish";
-                  openssh.authorizedKeys.keyFiles = [ ./authorized_keys ];
-                  extraGroups = [ "wheel" ];
-                };
-                users.users.root.openssh.authorizedKeys.keyFiles = [ ./authorized_keys ];
-                home-manager.useGlobalPkgs = true;
-                home-manager.useUserPackages = true;
-                home-manager.backupFileExtension = "backup";
-                home-manager.users.${user} =
-                  { config, ... }:
-                  {
-                    imports = [
-                      nix-index-database.hmModules.nix-index
-                      catppuccin.homeManagerModules.catppuccin
-                      ./modules/shared/home.nix
-                    ];
-                    catppuccin = {
-                      enable = true;
-                      flavor = "mocha";
-                    };
-                    programs.git = {
-                      inherit userEmail userName;
-                    };
-                    programs.atuin.settings.daemon.enabled = true;
-                  };
-                programs.fish.enable = true; # Otherwise our shell won't be installed correctly
-                services.tailscale.enable = true;
-                system.stateVersion = "23.05";
-              }
-            ];
-          };
         joip =
           let
             name = "Ólafur Bjarki Bogason";
@@ -310,6 +238,11 @@
               }
             ];
           };
+        biggimaus = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          inherit specialArgs;
+          modules = [ ./hosts/biggimaus ];
+        };
       };
     };
 }
