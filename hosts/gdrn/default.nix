efe80db5cbee5bec77c207b7d0f6f2f93df847e9
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-10-18 18:22:47 +0000

    move gdrn to hosts

--- hosts/gdrn/default.nix
+++ hosts/gdrn/default.nix
@@ -2,14 +2,32 @@
   lib,
   pkgs,
   config,
+  inputs,
   ...
 }:
+let
+  user = "genki";
+  userName = "Ólafur Bjarki Bogason";
+  userEmail = "olafur@genkiinstruments.com";
+in
 {
-  imports = [ ./disko-config.nix ];
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+    inputs.srvos.nixosModules.server
+    inputs.srvos.nixosModules.mixins-systemd-boot
+    inputs.srvos.nixosModules.mixins-terminfo
+    inputs.srvos.nixosModules.mixins-nix-experimental
+    inputs.srvos.nixosModules.mixins-trusted-nix-caches
+    inputs.srvos.nixosModules.roles-github-actions-runner
+    inputs.disko.nixosModules.disko
+    inputs.agenix.nixosModules.default
+    ./disko-config.nix
+    ../../modules/shared
+  ];
   disko.devices.disk.main.device = "/dev/disk/by-id/nvme-eui.002538b931a6cbb0";
 
-  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
 
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
   boot.initrd.availableKernelModules = [
     "nvme"
     "xhci_pci"
@@ -47,4 +65,66 @@
     cachix.cacheName = "genki";
     cachix.tokenFile = config.age.secrets.gdrn-github-runner-cachixToken.path;
   };
+  age = {
+    secrets = {
+      my-secret = {
+        symlink = true;
+        path = "/Users/${user}/Desktop/my-secret";
+        file = "${inputs.secrets}/my-secret.age";
+        mode = "644";
+        owner = "${user}";
+        group = "users";
+      };
+      atuin-key = {
+        symlink = true;
+        path = "/home/${user}/.local/share/atuin/key";
+        file = "${inputs.secrets}/atuin-key.age";
+        mode = "644";
+        owner = "${user}";
+        group = "users";
+      };
+      gdrn-github-runner-key = {
+        file = "${inputs.secrets}/gdrn-github-runner-key.age";
+      };
+      gdrn-github-runner-cachixToken = {
+        file = "${inputs.secrets}/gdrn-github-runner-cachixToken.age";
+      };
+    };
+  };
+  users.users.${user} = {
+    isNormalUser = true;
+    shell = "/run/current-system/sw/bin/fish";
+    description = "${userName}";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "docker"
+    ];
+    openssh.authorizedKeys.keyFiles = [ ../../authorized_keys ];
+  };
+  users.users.root.openssh.authorizedKeys.keyFiles = [ ../../authorized_keys ];
+  networking.hostName = "gdrn";
+  networking.hostId = "deadbeef";
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+  home-manager.backupFileExtension = "backup";
+  home-manager.users.${user} =
+    { config, ... }:
+    {
+      imports = [
+        inputs.nix-index-database.hmModules.nix-index
+        inputs.catppuccin.homeManagerModules.catppuccin
+        ../../modules/shared/home.nix
+      ];
+      catppuccin = {
+        enable = true;
+        flavor = "mocha";
+      };
+      programs.git = {
+        inherit userEmail userName;
+      };
+    };
+  programs.fish.enable = true;
+  services.openssh.extraConfig = ''AllowAgentForwarding yes'';
+  programs.ssh.startAgent = true;
 }
