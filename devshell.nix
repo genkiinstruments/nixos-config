823b9c169aebd1e702c57f7f8e6e2fb2a483b42b
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-10-18 19:31:15 +0000

    move to blueprint

---
+++ devshell.nix
@@ -0,0 +1,19 @@
+{ pkgs, perSystem }:
+pkgs.mkShell {
+  # Add build dependencies
+  packages = with pkgs; [
+    bashInteractive
+    git
+    nixos-anywhere
+    age
+    age-plugin-yubikey
+    age-plugin-fido2-hmac
+    perSystem.nix-darwin.darwin-rebuild
+  ];
+
+  # Add environment variables
+  env = { };
+
+  # Load custom bash code
+  shellHook = ''export EDITOR=nvim'';
+}
