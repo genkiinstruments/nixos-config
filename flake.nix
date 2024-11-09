9216af40cf07227c7e11b9aa22555d32beb73e06
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-11-09 17:45:12 +0000

    feat(flake.nix): flatten attrs

--- flake.nix
+++ flake.nix
@@ -5,58 +5,34 @@
     nixpkgs.follows = "srvos/nixpkgs"; # use the version of nixpkgs that has been tested with SrvOS
     blueprint.url = "github:numtide/blueprint";
     blueprint.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nix-darwin = {
-      url = "github:LnL7/nix-darwin/master";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin.url = "github:LnL7/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nixos-hardware.url = "github:nixos/nixos-hardware";
-    nix-homebrew = {
-      url = "github:zhaofengli-wip/nix-homebrew";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.nix-darwin.follows = "nix-darwin";
-    };
-    homebrew-bundle = {
-      url = "github:homebrew/homebrew-bundle";
-      flake = false;
-    };
-    homebrew-core = {
-      url = "github:homebrew/homebrew-core";
-      flake = false;
-    };
-    homebrew-cask = {
-      url = "github:homebrew/homebrew-cask";
-      flake = false;
-    };
-    homebrew-aerospace = {
-      url = "github:nikitabobko/homebrew-tap";
-      flake = false;
-    };
-    homebrew-zkondor = {
-      url = "github:zkondor/homebrew-dist";
-      flake = false;
-    };
-    homebrew-ssh-askpass = {
-      url = "github:theseal/homebrew-ssh-askpass";
-      flake = false;
-    };
-    nix-index-database = {
-      url = "github:Mic92/nix-index-database";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    nix-homebrew.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.inputs.nix-darwin.follows = "nix-darwin";
+    homebrew-bundle.url = "github:homebrew/homebrew-bundle";
+    homebrew-bundle.flake = false;
+    homebrew-core.url = "github:homebrew/homebrew-core";
+    homebrew-core.flake = false;
+    homebrew-cask.url = "github:homebrew/homebrew-cask";
+    homebrew-cask.flake = false;
+    homebrew-aerospace.url = "github:nikitabobko/homebrew-tap";
+    homebrew-aerospace.flake = false;
+    homebrew-zkondor.url = "github:zkondor/homebrew-dist";
+    homebrew-zkondor.flake = false;
+    homebrew-ssh-askpass.url = "github:theseal/homebrew-ssh-askpass";
+    homebrew-ssh-askpass.flake = false;
+    nix-index-database.url = "github:Mic92/nix-index-database";
+    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
     catppuccin.url = "github:catppuccin/nix";
     agenix.url = "github:ryantm/agenix";
-    secrets = {
-      url = "git+ssh://git@github.com/multivac61/nix-secrets.git";
-      flake = false;
-    };
+    secrets.url = "git+ssh://git@github.com/multivac61/nix-secrets.git";
+    secrets.flake = false;
   };
   outputs = inputs: inputs.blueprint { inherit inputs; };
 }
