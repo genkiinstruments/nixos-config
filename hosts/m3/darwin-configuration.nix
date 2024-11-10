c47d24ba8eed889857c35612487e942bacc6fa03
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-11-10 18:16:06 +0000

    feat: move shared darwin into separate files

--- hosts/m3/darwin-configuration.nix
+++ hosts/m3/darwin-configuration.nix
@@ -14,6 +14,7 @@
     inputs.nix-homebrew.darwinModules.nix-homebrew
     inputs.agenix.darwinModules.default
     inputs.self.modules.shared.default
+    inputs.self.modules.darwin.default
   ];
 
   nixpkgs.hostPlatform = "aarch64-darwin";
@@ -120,47 +121,5 @@
       "ColorSlurp" = 1287239339;
       "Numbers" = 409203825;
     };
-  };
-
-  system = {
-    stateVersion = 4;
-
-    defaults = {
-      NSGlobalDomain = {
-        AppleShowAllExtensions = true;
-        ApplePressAndHoldEnabled = false;
-
-        "com.apple.mouse.tapBehavior" = 1;
-        "com.apple.sound.beep.volume" = 0.0;
-        "com.apple.sound.beep.feedback" = 0;
-      };
-
-      dock = {
-        autohide = true;
-        show-recents = false;
-        tilesize = 72;
-        orientation = "left";
-      };
-
-      finder = {
-        _FXShowPosixPathInTitle = false;
-      };
-
-      trackpad = {
-        Clicking = true;
-        TrackpadThreeFingerDrag = true;
-      };
-    };
-
-    activationScripts.postActivation.text = ''
-      # normal minimum is 15 (225 ms)\ defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
-      defaults write -g InitialKeyRepeat -int 10
-      defaults write -g KeyRepeat -int 1
-    '';
-
-    # reload the settings and apply them without the need to logout/login
-    activationScripts.postUserActivation.text = "/System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u";
-    # https://github.com/LnL7/nix-darwin/issues/811
-    activationScripts.setFishAsShell.text = "dscl . -create /Users/olafur UserShell ${pkgs.fish}/bin/fish";
   };
 }
---
+++ modules/darwin/common/default.nix
@@ -0,0 +1,46 @@
+{ pkgs }:
+{
+
+  system = {
+    stateVersion = 4;
+
+    defaults = {
+      NSGlobalDomain = {
+        AppleShowAllExtensions = true;
+        ApplePressAndHoldEnabled = false;
+
+        "com.apple.mouse.tapBehavior" = 1;
+        "com.apple.sound.beep.volume" = 0.0;
+        "com.apple.sound.beep.feedback" = 0;
+      };
+
+      dock = {
+        autohide = true;
+        show-recents = false;
+        tilesize = 72;
+        orientation = "left";
+      };
+
+      finder = {
+        _FXShowPosixPathInTitle = false;
+      };
+
+      trackpad = {
+        Clicking = true;
+        TrackpadThreeFingerDrag = true;
+      };
+    };
+
+    activationScripts.postActivation.text = ''
+      # normal minimum is 15 (225 ms)\ defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+      defaults write -g InitialKeyRepeat -int 10
+      defaults write -g KeyRepeat -int 1
+    '';
+
+    # reload the settings and apply them without the need to logout/login
+    activationScripts.postUserActivation.text = "/System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u";
+    # https://github.com/LnL7/nix-darwin/issues/811
+    activationScripts.setFishAsShell.text = "dscl . -create /Users/olafur UserShell ${pkgs.fish}/bin/fish";
+  };
+
+}
