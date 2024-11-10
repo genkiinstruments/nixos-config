343dcfcbf1e374d4dbbf2ffb5cd075685f868c7b
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-11-10 18:16:06 +0000

    feat: move shared darwin into separate files

--- hosts/gkr/darwin-configuration.nix
+++ hosts/gkr/darwin-configuration.nix
@@ -12,6 +12,7 @@
     inputs.home-manager.darwinModules.home-manager
     inputs.agenix.darwinModules.default
     inputs.nix-homebrew.darwinModules.nix-homebrew
+    inputs.self.modules.darwin.default
     inputs.self.modules.shared.default
   ];
 
@@ -60,52 +61,5 @@
       SessionCreate = true;
       UserName = "genki";
     };
-  };
-  system = {
-    stateVersion = 4;
-
-    defaults = {
-      NSGlobalDomain = {
-        AppleShowAllExtensions = true;
-        ApplePressAndHoldEnabled = false;
-
-        # 120, 90, 60, 30, 12, 6, 2
-        KeyRepeat = 2;
-
-        # 120, 94, 68, 35, 25, 15
-        InitialKeyRepeat = 15;
-
-        "com.apple.mouse.tapBehavior" = 1;
-        "com.apple.sound.beep.volume" = 0.0;
-        "com.apple.sound.beep.feedback" = 0;
-      };
-
-      dock = {
-        autohide = true;
-        show-recents = false;
-        tilesize = 72;
-        orientation = "left";
-      };
-
-      finder = {
-        _FXShowPosixPathInTitle = false;
-      };
-
-      trackpad = {
-        Clicking = true;
-        TrackpadThreeFingerDrag = true;
-      };
-    };
-
-    keyboard = {
-      enableKeyMapping = true;
-      remapCapsLockToControl = true;
-    };
-
-    activationScripts.postActivation.text = ''
-      # normal minimum is 15 (225 ms)\ defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
-      defaults write -g InitialKeyRepeat -int 10
-      defaults write -g KeyRepeat -int 1
-    '';
   };
 }
