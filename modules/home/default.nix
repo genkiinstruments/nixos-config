7a6c698bddec1a78ff90ef4523635b602e22d9a9
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-02-05 17:55:12 +0000

    feat(fish.interactiveShellInit): move darwin specific stuff to separate file

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -251,45 +251,8 @@
         cat = "bat";
       };
       interactiveShellInit = ''
-        #-------------------------------------------------------------------------------
-        # SSH Agent
-        #-------------------------------------------------------------------------------
-        function __ssh_agent_is_started -d "check if ssh agent is already started"
-            if begin
-                    test -f $SSH_ENV; and test -z "$SSH_AGENT_PID"
-                end
-                source $SSH_ENV >/dev/null
-            end
-
-            if test -z "$SSH_AGENT_PID"
-                return 1
-            end
-
-            ${pkgs.openssh}/bin/ssh-add -l >/dev/null 2>&1
-            if test $status -eq 2
-                return 1
-            end
-        end
-
-        function __ssh_agent_start -d "start a new ssh agent"
-            ${pkgs.openssh}/bin/ssh-agent -c | sed 's/^echo/#echo/' >$SSH_ENV
-            chmod 600 $SSH_ENV
-            source $SSH_ENV >/dev/null
-            ${pkgs.openssh}/bin/ssh-add
-        end
-
-        if not test -d $HOME/.ssh
-            mkdir -p $HOME/.ssh
-            chmod 0700 $HOME/.ssh
-        end
-
-        if test -z "$SSH_ENV"
-            set -xg SSH_ENV $HOME/.ssh/environment
-        end
-
-        if not __ssh_agent_is_started
-            __ssh_agent_start
-        end
+        # Do not show any greeting
+        set fish_greeting
 
         #-------------------------------------------------------------------------------
         # Ghostty Shell Integration
@@ -315,9 +278,6 @@
             set -q INFOPATH; or set INFOPATH ""
             set -gx INFOPATH /opt/homebrew/share/info $INFOPATH
         end
-
-        # Do not show any greeting
-        set fish_greeting
 
         #-------------------------------------------------------------------------------
         # Atuin keybindings
