ee6e7d24de7d92793be01ec989440609739544ba
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-02-05 17:55:12 +0000

    feat(fish.interactiveShellInit): move darwin specific stuff to separate file

--- hosts/m3/darwin-configuration.nix
+++ hosts/m3/darwin-configuration.nix
@@ -69,6 +69,47 @@
     serverAliveInterval = 900;
     extraConfig = "SetEnv TERM=xterm-256color";
   };
+  home-manager.users.olafur.programs.fish.interactiveShellInit = ''
+    #-------------------------------------------------------------------------------
+    # SSH Agent
+    #-------------------------------------------------------------------------------
+    function __ssh_agent_is_started -d "check if ssh agent is already started"
+        if begin
+                test -f $SSH_ENV; and test -z "$SSH_AGENT_PID"
+            end
+            source $SSH_ENV >/dev/null
+        end
+
+        if test -z "$SSH_AGENT_PID"
+            return 1
+        end
+
+        ${pkgs.openssh}/bin/ssh-add -l >/dev/null 2>&1
+        if test $status -eq 2
+            return 1
+        end
+    end
+
+    function __ssh_agent_start -d "start a new ssh agent"
+        ${pkgs.openssh}/bin/ssh-agent -c | sed 's/^echo/#echo/' >$SSH_ENV
+        chmod 600 $SSH_ENV
+        source $SSH_ENV >/dev/null
+        ${pkgs.openssh}/bin/ssh-add
+    end
+
+    if not test -d $HOME/.ssh
+        mkdir -p $HOME/.ssh
+        chmod 0700 $HOME/.ssh
+    end
+
+    if test -z "$SSH_ENV"
+        set -xg SSH_ENV $HOME/.ssh/environment
+    end
+
+    if not __ssh_agent_is_started
+        __ssh_agent_start
+    end
+  '';
 
   users.users.olafur = {
     isHidden = false;
