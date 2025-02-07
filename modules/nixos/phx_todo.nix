98cf544d1db9540b2cbdd11fff5aad2fed7c6bf9
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-02-07 00:35:16 +0000

    feat(t1): more nginx work

--- modules/nixos/phx_todo.nix
+++ modules/nixos/phx_todo.nix
@@ -177,7 +177,8 @@ in
         proxyPass = "http://localhost:${toString cfg.port}";
         proxyWebsockets = true; # Important for Phoenix LiveView
         extraConfig = ''
-          proxy_http_version 1.1;
+          proxy_set_header Upgrade $http_upgrade;
+          proxy_set_header Connection $connection_upgrade;
           proxy_set_header Host $host;
           proxy_set_header X-Real-IP $remote_addr;
           proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
