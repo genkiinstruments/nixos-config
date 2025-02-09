214594097279c4c8e645222625741f9d5c0f6822
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-02-09 22:59:31 +0000

    feat(m3vm): add hm programs.ssh to deal not having to always authenticate 🙃

--- hosts/m3vm/users/genki.nix
+++ hosts/m3vm/users/genki.nix
@@ -13,4 +13,13 @@
   programs.fish.interactiveShellInit = ''
     export NIX_LD=$(nix eval --extra-experimental-features nix-command --impure --raw --expr 'let pkgs = import <nixpkgs> {}; NIX_LD = pkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker"; in NIX_LD')
   '';
+  programs.ssh = {
+    controlMaster = "auto";
+    controlPath = "/tmp/ssh-%u-%r@%h:%p";
+    controlPersist = "1800";
+    forwardAgent = true;
+    addKeysToAgent = "yes";
+    serverAliveInterval = 900;
+    extraConfig = "SetEnv TERM=xterm-256color";
+  };
 }
