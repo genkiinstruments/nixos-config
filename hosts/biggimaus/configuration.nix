691c02af114350e42eb8fbb0209e4c5cb115b105
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-03-14 21:42:59 +0000

    fix(biggimaus): remove unused home-manager module where no user lives

--- hosts/biggimaus/configuration.nix
+++ hosts/biggimaus/configuration.nix
@@ -11,7 +11,6 @@
     inputs.srvos.nixosModules.mixins-trusted-nix-caches
     inputs.disko.nixosModules.disko
     flake.modules.shared.default
-    flake.modules.shared.home-manager
     flake.nixosModules.common
     ./disko-config.nix
   ];
