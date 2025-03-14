03d5705b62468c41c017898c49a542e712d3c5d9
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-03-14 21:44:39 +0000

    feat(g): try to add graphical stuff to

--- hosts/g/configuration.nix
+++ hosts/g/configuration.nix
@@ -65,4 +65,92 @@
   };
 
   system.stateVersion = "24.11";
+
+  programs.nix-ld.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.mutableUsers = false;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages =
+    with pkgs;
+    [
+      cachix
+      gnumake
+      killall
+      niv
+      xclip
+      magic-wormhole-rs
+      git
+      alacritty
+      open-vm-tools
+      networkmanagerapplet
+      gnome-tweaks
+      dconf-editor
+      ghostty
+      rofi
+
+      # For hypervisors that support auto-resizing, this script forces it.
+      # I've noticed not everyone listens to the udev events so this is a hack.
+      (writeShellScriptBin "xrandr-auto" ''
+        xrandr --output Virtual-1 --auto
+      '')
+    ]
+    ++ [
+      # This is needed for the vmware user tools clipboard to work.
+      # You can test if you don't need this by deleting this and seeing
+      # if the clipboard sill works.
+      shared-mime-info
+      xdg-utils
+      gtkmm3
+    ];
+
+  services.displayManager.defaultSession = "none+i3";
+
+  services.xserver = {
+    enable = true;
+    # displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+
+    desktopManager = {
+      xterm.enable = false;
+      wallpaper.mode = "fill";
+    };
+
+    displayManager = {
+      lightdm.enable = true;
+
+      # AARCH64: For now, on Apple Silicon, we must manually set the
+      # display resolution. This is a known issue with VMware Fusion.
+      sessionCommands = ''
+        ${pkgs.xorg.xset}/bin/xset r rate 200 40
+      '';
+    };
+
+    windowManager.i3.enable = true;
+  };
+
+  # GNOME packages
+  environment.gnome.excludePackages = with pkgs; [
+    epiphany # web browser
+    totem # video player
+    geary # email client
+    evince # document viewer
+    # Add other GNOME packages you want to exclude
+  ];
+
+  # We need an XDG portal for various applications to work properly,
+  # such as Flatpak applications.
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    config.common.default = "*";
+  };
+
+  services.udev.packages = [
+    pkgs.yubikey-personalization
+    pkgs.libfido2
+  ];
+
 }
---
+++ hosts/g/users/genki.nix
@@ -0,0 +1,20 @@
+{
+  flake,
+  ...
+}:
+{
+  imports = [
+    flake.homeModules.default
+    flake.homeModules.nvim
+  ];
+
+  programs.ssh = {
+    controlMaster = "auto";
+    controlPath = "/tmp/ssh-%u-%r@%h:%p";
+    controlPersist = "1800";
+    forwardAgent = true;
+    addKeysToAgent = "yes";
+    serverAliveInterval = 900;
+    extraConfig = "SetEnv TERM=xterm-256color";
+  };
+}
