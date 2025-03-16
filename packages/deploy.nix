308e7c4e0eff0ddf1a2c0ba84b45c50120c4d8b3
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-03-16 17:59:28 +0000

    fix(deploy): better implementation

--- packages/deploy.nix
+++ packages/deploy.nix
@@ -1,9 +1,15 @@
-{ pkgs, flake, ... }:
+{
+  pkgs,
+  inputs,
+  ...
+}:
+let
+  flakeInputs = map (v: v.outPath) (builtins.attrValues inputs.self.inputs);
+in
 pkgs.writeShellApplication {
   name = "deploy";
   runtimeInputs = with pkgs; [
     nix
-    rsync
     openssh
   ];
   text = ''
@@ -17,19 +23,24 @@ pkgs.writeShellApplication {
     USER="$2"
     echo "Deploying $NAME for user $USER"
 
-    rsync -av --delete "${toString flake}/" "root@$NAME:/etc/nixos-config"
+    # Copy all flake inputs to the target using ssh-ng
+    nix copy ${toString flakeInputs} --to ssh-ng://root@"$NAME"
 
+    # Copy the flake itself using ssh-ng
+    nix copy ${inputs.self} --to ssh-ng://root@"$NAME"
+
+    # Run the appropriate rebuild command
     ssh -t "root@$NAME" "
       if [ -d /Applications ]; then
         # macOS
-        chown -R $USER:staff /etc/nixos-config/home/nvim
-        chmod -R u+w /etc/nixos-config/home/nvim
-        nix run nixpkgs#darwin-rebuild -- switch --flake '/etc/nixos-config#$NAME'
+        chown -R $USER:staff /etc/nixos-config/home/nvim 2>/dev/null || true
+        chmod -R u+w /etc/nixos-config/home/nvim 2>/dev/null || true
+        darwin-rebuild switch --flake '${inputs.self}#$NAME'
       else
         # NixOS
-        chown -R $USER:users /etc/nixos-config/home/nvim
-        chmod -R u+w /etc/nixos-config/home/nvim
-        nix run nixpkgs#nixos-rebuild -- switch --flake '/etc/nixos-config#$NAME'
+        chown -R $USER:users /etc/nixos-config/home/nvim 2>/dev/null || true
+        chmod -R u+w /etc/nixos-config/home/nvim 2>/dev/null || true
+        nixos-rebuild switch --flake '${inputs.self}#$NAME'
       fi
     "
   '';
