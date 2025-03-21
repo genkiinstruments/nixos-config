fb58e6045ddaefc1fbae596d0acc2b8c2391eb2e
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-03-21 19:04:27 +0000

    fix: try to fix error with fish on darwin

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -3,4 +3,5 @@
   programs.fish.enable = true; # Otherwise our shell won't be installed correctly
   environment.shells = with pkgs; [ fish ];
   home-manager.backupFileExtension = "backup";
+  environment.variables.SHELL = "${pkgs.fish}/bin/fish";
 }
