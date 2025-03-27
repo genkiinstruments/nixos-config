195ee2ba560d96424769b1bad96d4493466ffc3f
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-03-27 16:02:16 +0000

    feat: simplify builders.nix file

--- modules/shared/builders.nix
+++ modules/shared/builders.nix
@@ -1,91 +1,81 @@
-_: {
-  nix = {
-    settings.trusted-users = [ "nix-ssh" ];
-    distributedBuilds = true;
-    buildMachines = [
-      {
-        hostName = "gdrn";
-        sshUser = "nix-ssh";
-        protocol = "ssh-ng";
-        systems = [ "x86_64-linux" ];
-        maxJobs = 32;
-        supportedFeatures = [
-          "nixos-test"
-          "benchmark"
-          "big-parallel"
-          "kvm"
-        ];
-      }
-      {
-        hostName = "x";
-        sshUser = "nix-ssh";
-        protocol = "ssh-ng";
-        systems = [ "x86_64-linux" ];
-        maxJobs = 32;
-        supportedFeatures = [
-          "nixos-test"
-          "benchmark"
-          "big-parallel"
-          "kvm"
-        ];
-      }
-      {
-        hostName = "v1";
-        sshUser = "nix-ssh";
-        protocol = "ssh-ng";
-        systems = [ "aarch64-linux" ];
-        maxJobs = 14;
-        supportedFeatures = [
-          "nixos-test"
-          "benchmark"
-          "big-parallel"
-          "kvm"
-        ];
-      }
-      {
-        hostName = "gkr";
-        sshUser = "nix-ssh";
-        protocol = "ssh-ng";
-        systems = [ "aarch64-darwin" ];
-        maxJobs = 8;
-        supportedFeatures = [
-          "benchmark"
-          "big-parallel"
-        ];
-      }
-    ];
-  };
-  programs.ssh.extraConfig = ''
-    Host gdrn gdrn.tail01dbd.ts.net
-      User nix-ssh
-      HostName gdrn.tail01dbd.ts.net
-      StrictHostKeyChecking accept-new
-      BatchMode yes
-      PubkeyAuthentication yes
-      IdentitiesOnly yes
-      
-    Host v1 v1.tail01dbd.ts.net
-      User nix-ssh
-      HostName v1.tail01dbd.ts.net
-      StrictHostKeyChecking accept-new
-      BatchMode yes
-      PubkeyAuthentication yes
-      IdentitiesOnly yes
+{
+  lib,
+  flake,
+  ...
+}:
+let
+  inherit (flake.lib) tailnet;
 
-    Host x x.tail01dbd.ts.net
+  mkBuilder =
+    {
+      hostName,
+      systems,
+      maxJobs,
+    }:
+    {
+      inherit
+        hostName
+        systems
+        maxJobs
+        ;
+      sshUser = "nix-ssh";
+      protocol = "ssh-ng";
+      supportedFeatures = [
+        "nixos-test"
+        "benchmark"
+        "big-parallel"
+        "kvm"
+      ];
+    };
+
+  mkSshConfig = hostName: ''
+    Host ${hostName} ${hostName}.${tailnet}
       User nix-ssh
-      HostName x.tail01dbd.ts.net
-      StrictHostKeyChecking accept-new
-      BatchMode yes
-      PubkeyAuthentication yes
-      IdentitiesOnly yes
-      
-    Host gkr gkr.tail01dbd.ts.net
-      User nix-ssh
-      HostName gkr.tail01dbd.ts.net
+      HostName ${hostName}.${tailnet}
       StrictHostKeyChecking accept-new
       BatchMode yes
       PubkeyAuthentication yes
       IdentitiesOnly yes
   '';
+
+  builders = [
+    (mkBuilder {
+      hostName = "gdrn";
+      systems = [ "x86_64-linux" ];
+      maxJobs = 32;
+    })
+    (mkBuilder {
+      hostName = "x";
+      systems = [ "x86_64-linux" ];
+      maxJobs = 32;
+    })
+    (mkBuilder {
+      hostName = "v1";
+      systems = [ "aarch64-linux" ];
+      maxJobs = 14;
+    })
+    (mkBuilder {
+      hostName = "pbt";
+      systems = [ "aarch64-linux" ];
+      maxJobs = 14;
+    })
+    (mkBuilder {
+      hostName = "gkr";
+      systems = [ "aarch64-darwin" ];
+      maxJobs = 8;
+    })
+  ];
+
+  hostNames = map (builder: builder.hostName) builders;
+  sshConfigs = lib.concatMapStrings mkSshConfig hostNames;
+in
+{
+  nix = {
+    settings.trusted-users = [ "nix-ssh" ];
+    distributedBuilds = true;
+    buildMachines = builders;
+  };
+
+  programs.ssh.extraConfig = sshConfigs;
 }
+
