7808b931b20bb30205e457a709fc54ff2e4589d3
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-04-20 12:02:40 +0000

    feat(m3): lg as lazygit with

--- hosts/m3/users/olafur.nix
+++ hosts/m3/users/olafur.nix
@@ -34,7 +34,7 @@
         mkdir -p $HOME/.ssh
         chmod 0700 $HOME/.ssh
     end
-    
+
     # More reliable ssh-agent detection function
     function __ssh_agent_is_started -d "check if ssh agent is already started"
         # First, try to use the environment file if it exists
@@ -78,6 +78,17 @@
     # Only start ssh-agent if one isn't already running
     if not __ssh_agent_is_started
         __ssh_agent_start
+    end
+
+    function lg
+      set -x LAZYGIT_NEW_DIR_FILE ~/.lazygit/newdir
+
+      lazygit $argv
+
+      if test -f $LAZYGIT_NEW_DIR_FILE
+          cd (cat $LAZYGIT_NEW_DIR_FILE)
+          rm -f $LAZYGIT_NEW_DIR_FILE > /dev/null
+      end
     end
   '';
 }
