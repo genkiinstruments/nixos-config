995b3e570fc973c0f7b1152657a51e52f9b35c83
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-04-23 13:19:29 +0000

    feat: add matt to authorized keys

--- modules/nixos/ssh-serve.nix
+++ modules/nixos/ssh-serve.nix
@@ -3,6 +3,9 @@ _: {
     sshServe.protocol = "ssh-ng";
     sshServe.enable = true;
     sshServe.write = true;
+    sshServe.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC/HMAT/nOa8F5LrFebnG7wk1o/K0Rx1HdDoFYxvLSef root@p4" # matt
+    ];
     # Using Tailscale SSH for authentication so no need to store keys
     settings.trusted-users = [ "nix-ssh" ];
   };
