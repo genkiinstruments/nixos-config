df40b8d22dfbffcfd865b7f78b80094a21be2943
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-04-30 21:30:07 +0000

    feat(tailscale): openFirewall and useRoutingFeatures = both

--- modules/nixos/common/default.nix
+++ modules/nixos/common/default.nix
@@ -1,4 +1,6 @@
 {
   networking.firewall.trustedInterfaces = [ "tailscale0" ];
   networking.firewall.allowPing = true;
+  services.tailscale.openFirewall = true;
+  services.tailscale.useRoutingFeatures = "both";
 }
