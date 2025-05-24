edce03469d7698a7a00d5a904ce1283de5dcbddd
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-05-24 11:32:40 +0000

    feat: Isolate the clipboard handling to a single script

--- modules/home/config/scripts/osc52.sh
+++ modules/home/config/scripts/osc52.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 # A simple script to copy text to clipboard using OSC52 escape sequence
 # This works even over SSH connections
 
@@ -10,3 +11,4 @@ b64=$(echo -n "$buf" | base64 | tr -d '\n')
 
 # Send the OSC52 sequence to the terminal
 printf "\033]52;c;%s\007" "$b64"
+
