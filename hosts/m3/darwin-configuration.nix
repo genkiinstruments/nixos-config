452bdefb75492fd2902dfad0bf6066d8edc2931d
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-05-24 14:08:03 +0000

    feat(m3): remove builders for now (super annoying)

--- hosts/m3/darwin-configuration.nix
+++ hosts/m3/darwin-configuration.nix
@@ -11,7 +11,6 @@
     inputs.srvos.darwinModules.mixins-terminfo
     inputs.agenix.darwinModules.default
     flake.modules.shared.default
-    flake.modules.shared.builders
     flake.modules.shared.home-manager
     flake.darwinModules.common
   ];
