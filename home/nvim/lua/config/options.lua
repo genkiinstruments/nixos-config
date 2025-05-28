d8dc93c467db6ff390340a30c54ddff5b2960090
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-05-28 12:40:16 +0000

    fix(nvim): visual selection copy did not work with previous version

--- home/nvim/lua/config/options.lua
+++ home/nvim/lua/config/options.lua
@@ -7,9 +7,7 @@ vim.opt.swapfile = false
 -- Clipboard
 if vim.env.SSH_TTY then
     vim.opt.clipboard:append("unnamedplus")
-    local function paste()
-        return vim.split(vim.fn.getreg(""), "\n")
-    end
+
     vim.g.clipboard = {
         name = "OSC 52",
         copy = {
@@ -17,11 +15,12 @@ if vim.env.SSH_TTY then
             ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
         },
         paste = {
-            ["+"] = paste,
-            ["*"] = paste,
+            ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+            ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
         },
     }
 end
+
 --  https://old.reddit.com/r/neovim/comments/1ajpdrx/lazyvim_weird_live_grep_root_dir_functionality_in/
 -- Type :LazyRoot in the directory you're in and that will show you the root_dir that will be used for the root_dir search commands. The reason you're experiencing this behavior is because your subdirectories contain some kind of root_dir pattern for the LSP server attached to the buffer.
 vim.g.root_spec = { "cwd" }
