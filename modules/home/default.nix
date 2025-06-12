600ea2ffc72af71d5972b11b1d7a8a89f4f18337
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-06-12 10:16:32 +0000

    chore(home): use bash formatting

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -226,19 +226,20 @@
       silent = true;
       nix-direnv.enable = true; # Adds FishIntegration automatically
       config.warn_timeout = "30m";
-      stdlib = ''
-        # Avoid cluttering project directories which often conflicts with tooling, e.g., `mix`
-        # https://github.com/direnv/direnv/wiki/Customizing-cache-location
-        # Centralize direnv layouts in $HOME/.cache/direnv/layouts
-        : ''${XDG_CACHE_HOME:=$HOME/.cache}
-        declare -A direnv_layout_dirs
-        direnv_layout_dir() {
-          echo "''${direnv_layout_dirs[$PWD]:=$(
-            echo -n "$XDG_CACHE_HOME"/direnv/layouts/
-            echo -n "$PWD" | shasum | cut -d ' ' -f 1
-          )}"
-        }
-      '';
+      stdlib = # bash
+        ''
+          # Avoid cluttering project directories which often conflicts with tooling, e.g., `mix`
+          # https://github.com/direnv/direnv/wiki/Customizing-cache-location
+          # Centralize direnv layouts in $HOME/.cache/direnv/layouts
+          : ''${XDG_CACHE_HOME:=$HOME/.cache}
+          declare -A direnv_layout_dirs
+          direnv_layout_dir() {
+            echo "''${direnv_layout_dirs[$PWD]:=$(
+              echo -n "$XDG_CACHE_HOME"/direnv/layouts/
+              echo -n "$PWD" | shasum | cut -d ' ' -f 1
+            )}"
+          }
+        '';
     };
     fish = {
       enable = true;
