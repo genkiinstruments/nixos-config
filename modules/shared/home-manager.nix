22b6387604784c1f8dafa4dbdac2bf228428d838
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-06-20 14:54:42 +0000

    chore(modules/shared): mvim to all home-manager managed systems

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -1,9 +1,10 @@
 { pkgs, perSystem, ... }:
 {
-  environment.systemPackages = [ perSystem.self.mvim ];
-
   programs.fish.enable = true; # Otherwise our shell won't be installed correctly
   environment.shells = with pkgs; [ fish ];
   home-manager.backupFileExtension = "backup";
   environment.variables.SHELL = "${pkgs.fish}/bin/fish";
+  
+  # Add mvim by default to all systems
+  environment.systemPackages = [ perSystem.self.mvim ];
 }
