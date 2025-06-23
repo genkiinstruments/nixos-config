0f9b2cbd63a3d93125e4d5c83903d2f8bcbb87bb
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-06-23 14:58:58 +0000

    chore: nix fmt

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -4,7 +4,7 @@
   environment.shells = with pkgs; [ fish ];
   home-manager.backupFileExtension = "backup";
   environment.variables.SHELL = "${pkgs.fish}/bin/fish";
-  
+
   # Add mvim by default to all systems
   environment.systemPackages = [ perSystem.self.mvim ];
 }
