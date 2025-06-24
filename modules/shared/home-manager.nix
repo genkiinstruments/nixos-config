81d660bd96044a75d34eca1281689a20258836b1
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-06-24 15:03:32 +0000

    feat(modules/shared): don't install mvim everywhere

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -1,10 +1,7 @@
-{ pkgs, perSystem, ... }:
+{ pkgs, ... }:
 {
   programs.fish.enable = true; # Otherwise our shell won't be installed correctly
   environment.shells = with pkgs; [ fish ];
   home-manager.backupFileExtension = "backup";
   environment.variables.SHELL = "${pkgs.fish}/bin/fish";
-
-  # Add mvim by default to all systems
-  environment.systemPackages = [ perSystem.self.mvim ];
 }
