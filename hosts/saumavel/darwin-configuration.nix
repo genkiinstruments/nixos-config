f243baaab583e08e4d7c755f9822e2e037cf759a
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-06-24 11:51:29 +0000

    feat(saumavel): init, without kari's special neovim config

---
+++ hosts/saumavel/darwin-configuration.nix
@@ -0,0 +1,99 @@
+{
+  flake,
+  pkgs,
+  lib,
+  inputs,
+  ...
+}:
+{
+  imports = [
+    inputs.srvos.darwinModules.common
+    inputs.srvos.darwinModules.mixins-terminfo
+    flake.modules.darwin.default
+    flake.modules.shared.default
+    flake.modules.shared.home-manager
+  ];
+
+  # Add the unfree configuration here
+  nixpkgs.config.allowUnfreePredicate =
+    pkg:
+    builtins.elem (lib.getName pkg) [
+      "copilot.vim"
+    ];
+
+  nixpkgs.hostPlatform = "aarch64-darwin";
+
+  system.primaryUser = "saumavel";
+
+  users.users.saumavel = {
+    isHidden = false;
+    home = "/Users/saumavel";
+    shell = pkgs.fish;
+  };
+
+  # Fix nixbld group ID issue
+  ids.gids.nixbld = 350;
+
+  # Modified homebrew configuration to update but not remove packages
+  homebrew = {
+    enable = false;
+    onActivation = {
+      autoUpdate = true; # Update Homebrew and formulae
+      upgrade = true; # Upgrade outdated packages
+      cleanup = "none"; # Don't remove any packages not in the list
+    };
+    brews = [
+      "nvm"
+      "node"
+      "clang-format"
+      "hashcat"
+      "qemu"
+      "luarocks"
+      "biber"
+      "texlive"
+      "xdotool"
+    ];
+    casks = [
+      # MEGA UTILITIES
+      "raycast"
+      "alt-tab"
+      "karabiner-elements"
+      "shortcat"
+      "screen-studio"
+
+      # UTILITIES
+      "keyboardcleantool"
+      "logi-options+"
+      "the-unarchiver"
+      "postman"
+
+      # TERMINALS
+      "ghostty"
+      "kitty"
+
+      # WORK
+      "obsidian"
+      "slack"
+      "linear-linear"
+
+      # BROWSERS
+      "arc"
+
+      # CHAT
+      "messenger"
+
+      # FUN
+      "bitwig-studio"
+      "plex"
+
+      # IDE´s
+      "zed"
+      "visual-studio-code"
+    ];
+    masApps = {
+      # `nix run nixpkgs #mas -- search <app name>`
+      "Keynote" = 409183694;
+      "Pages" = 409201541;
+    };
+  };
+}
---
+++ hosts/saumavel/users/saumavel.nix
@@ -0,0 +1,103 @@
+{
+  flake,
+  pkgs,
+  lib,
+  ...
+}:
+{
+  imports = [
+    flake.modules.home.default
+    flake.modules.home.mvim
+  ];
+
+  # Enable mvim with home-manager integration and custom config path
+  programs.mvim = {
+    enable = true;
+    configPath = "/Users/saumavel/genkiinstruments/nixos-config/home/nvim";
+  };
+
+  # XDG Base Directory specification configuration
+  # Manages application configurations and default applications
+  xdg = {
+    enable = true;
+    # Application-specific configuration files
+    configFile = {
+      # Ghostty configuration
+      "ghostty/config".source = ./ghostty;
+      # Keyboard customization
+      "karabiner/karabiner.json".source = ./karabiner.json;
+    };
+    # Default applications for file types;
+    mimeApps.defaultApplications = {
+      # Web application
+      "text/html" = "arc.desktop";
+      # plain text files
+      "text/plain" = "nvim.desktop";
+      # PDF
+      "application/pdf" = "mupdf.desktop";
+    };
+  };
+
+  # NOTE: START HERE: Install packages that are only available in your user environment.
+  # https://home-manager-options.extranix.com/
+  programs = {
+    bash.enable = true;
+
+    fish.shellAliases.cppbuild = "g++ -std=c++17 -Wall -Wextra";
+
+    kitty = {
+      enable = true;
+      shellIntegration.enableFishIntegration = true;
+      settings = {
+        confirm_os_window_close = -0;
+        copy_on_select = true;
+        clipboard_control = "write-clipboard read-clipboard write-primary read-primary";
+      };
+      font = {
+        size = 16.0;
+        name = "JetBrainsMono Nerd Font";
+      };
+    };
+
+    tmux = {
+      prefix = "C-a";
+      keyMode = "vi";
+      extraConfig = ''
+        set -g default-command ${pkgs.fish}/bin/fish
+
+        # remappa " og % í þægilegri takka
+        bind i split-window -h
+        bind u split-window -v
+
+        # alt H & L til að navigatea milli windows
+        bind -n M-H previous-window
+        bind -n M-L previous-window
+
+        # Open panes in current directory
+        bind '"' split-window -v -c "#{pane_current_path}"
+        bind % split-window -h -c "#{pane_current_path}"
+
+        set -g status-position top
+      '';
+      plugins = with pkgs.tmuxPlugins; [
+        tmux-fzf
+        vim-tmux-navigator
+        yank
+        sensible
+      ];
+    };
+
+    git = {
+      userName = lib.mkForce "saumavel";
+      userEmail = lib.mkForce "saumavel@gmail.com";
+      delta.enable = true;
+    };
+  };
+
+  # NOTE: Use this to add packages available everywhere on your system
+  # $search nixpkgs {forrit}
+  # https://search.nixos.org/packages
+  home.packages = with pkgs; [
+    pstree # Display running processes as a tree
+  ];
+}
