0d3620a5c8c015e0e12b60938e9c7d62e8195019
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-06-25 12:47:54 +0000

    feat(d): keyboard remapping

--- hosts/d/darwin-configuration.nix
+++ hosts/d/darwin-configuration.nix
@@ -22,6 +22,54 @@
   };
   system.primaryUser = "genki";
 
+  # Keyboard remapping configuration
+  system.keyboard = {
+    enableKeyMapping = true;
+    remapCapsLockToEscape = true;
+  };
+
+  # Additional keyboard settings via system defaults
+  system.defaults = {
+    NSGlobalDomain = {
+      # Swap Option and Command keys for USB keyboards
+      "com.apple.keyboard.fnState" = false;
+    };
+    
+    # Keyboard modifier key mappings
+    ".GlobalPreferences" = {
+      "com.apple.keyboard.modifiermapping.1452-544-0" = [
+        {
+          HIDKeyboardModifierMappingSrc = 30064771296;  # Left Option
+          HIDKeyboardModifierMappingDst = 30064771299;  # Left Command
+        }
+        {
+          HIDKeyboardModifierMappingSrc = 30064771299;  # Left Command  
+          HIDKeyboardModifierMappingDst = 30064771296;  # Left Option
+        }
+        {
+          HIDKeyboardModifierMappingSrc = 30064771300;  # Right Option
+          HIDKeyboardModifierMappingDst = 30064771303;  # Right Command
+        }
+        {
+          HIDKeyboardModifierMappingSrc = 30064771303;  # Right Command
+          HIDKeyboardModifierMappingDst = 30064771300;  # Right Option
+        }
+      ];
+      
+      # Apple keyboard mappings (swap Control and Globe/Fn)
+      "com.apple.keyboard.modifiermapping.1452-0-0" = [
+        {
+          HIDKeyboardModifierMappingSrc = 30064771297;  # Left Control
+          HIDKeyboardModifierMappingDst = 1095216660483; # Globe/Fn key
+        }
+        {
+          HIDKeyboardModifierMappingSrc = 1095216660483; # Globe/Fn key
+          HIDKeyboardModifierMappingDst = 30064771297;  # Left Control
+        }
+      ];
+    };
+  };
+
   # NOTE: Here you can install packages from brew
   homebrew = {
     enable = true;
