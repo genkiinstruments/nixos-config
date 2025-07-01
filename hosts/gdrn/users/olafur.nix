b86976e0e70bb148cc1f050d2b3de5f7a7e1d048
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-07-01 13:48:23 +0000

    feat(gdrn): git creds

--- hosts/gdrn/users/olafur.nix
+++ hosts/gdrn/users/olafur.nix
@@ -10,6 +10,11 @@
   programs.fish.shellAliases.n = "mvim";
   home.sessionVariables.EDITOR = lib.mkDefault "mvim";
   home.packages = [ perSystem.self.mvim ];
+  programs.git = {
+    userEmail = "olafur@genkiinstruments.com";
+    userName = "multivac61";
+    extraConfig.github.user = "multivac61";
+  };
 
   programs.ssh = {
     controlMaster = "auto";
