1821eb2f308499b26e14d7929cc3c99508d6bb22
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-07-07 10:00:13 +0000

    fix(pbt): asahi-fwextract fix

--- hosts/pbt/apple-silicon-support/packages/asahi-fwextract/default.nix
+++ hosts/pbt/apple-silicon-support/packages/asahi-fwextract/default.nix
@@ -9,6 +9,9 @@ python3.pkgs.buildPythonApplication rec {
   pname = "asahi-fwextract";
   version = "0.7.8";
 
+  pyproject = true;
+  build-system = [ python3.pkgs.setuptools ];
+
   # tracking version: https://packages.fedoraproject.org/pkgs/asahi-installer/python3-asahi_firmware/
   src = fetchFromGitHub {
     owner = "AsahiLinux";
@@ -24,8 +27,6 @@ python3.pkgs.buildPythonApplication rec {
       --replace '"tar"' '"${gnutar}/bin/tar"' \
       --replace '"xf"' '"-x", "-I", "${gzip}/bin/gzip", "-f"'
   '';
-
-  nativeBuildInputs = [ python3.pkgs.setuptools ];
 
   doCheck = false;
 }
