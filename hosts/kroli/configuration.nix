4f7e65be6a84ec4d35ddcb5a56d77b40bd6b191e
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-08-06 10:57:00 +0000

    feat(kroli): add user

--- hosts/kroli/configuration.nix
+++ hosts/kroli/configuration.nix
@@ -1,6 +1,7 @@
 {
   inputs,
   flake,
+  pkgs,
   ...
 }:
 {
@@ -12,6 +13,7 @@
     inputs.disko.nixosModules.disko
     inputs.nixos-facter-modules.nixosModules.facter
     flake.modules.shared.default
+    flake.modules.shared.home-manager
     flake.modules.nixos.default
     ./disk-config.nix
   ];
@@ -23,4 +25,25 @@
   facter.reportPath = ./facter.json;
 
   users.users.root.initialHashedPassword = "$y$j9T$xA3OJK4WPx3Gu80.nTV6h/$DsBKf3OL11/d9bOAQmSVbgf2H2Ue4FAwhPLcatF0tX3";
+
+  security.sudo.wheelNeedsPassword = false;
+  users.mutableUsers = false;
+  # We are using zfs: https://github.com/atuinsh/atuin/issues/952#issuecomment-2199964530
+  home-manager.users.olafur.programs.atuin.daemon.enable = true;
+  users.users.olafur = {
+    isNormalUser = true;
+    description = "olafur";
+    shell = pkgs.fish;
+    hashedPassword = "$6$UIOsLjI24UeaovvG$SVVrXdpnepj/w1jhmYNdpPpmcgkcXsMBcAkqrcIL5yCCYDAkc/8kblyzuBLyK6PnJqR1JxZ7XtlWyCJwWhGrw.";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "plugdev"
+      "dialout"
+      "video"
+      "inputs"
+    ];
+    openssh.authorizedKeys.keyFiles = [ "${flake}/authorized_keys" ];
+  };
+  nix.settings.trusted-users = [ "olafur" ];
 }
