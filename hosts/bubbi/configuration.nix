89ca7b102b1c3e12f12c39d1a23e3a307dbc67ae
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-08-12 18:28:22 +0000

    feat(bubbi): root access to frontpanel

--- hosts/bubbi/configuration.nix
+++ hosts/bubbi/configuration.nix
@@ -51,7 +51,7 @@
   # USB device access for katla-frontpanel
   services.udev.extraRules = ''
     # Genki katla-frontpanel USB device (both product IDs) - NixOS style
-    SUBSYSTEM=="usb", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="27dd", MODE="0664", GROUP="users", TAG+="uaccess"
-    SUBSYSTEM=="usb", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="20b1", MODE="0664", GROUP="users", TAG+="uaccess"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="27dd", MODE="0666", TAG+="uaccess"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="20b1", MODE="0666", TAG+="uaccess"
   '';
 }
