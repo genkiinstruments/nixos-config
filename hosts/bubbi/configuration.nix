5580d496667d78f32b4b8043149c75885149870b
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-08-12 18:23:50 +0000

    feat(bubbi): init

---
+++ hosts/bubbi/configuration.nix
@@ -0,0 +1,53 @@
+{
+  inputs,
+  flake,
+  ...
+}:
+{
+  imports = [
+    inputs.srvos.nixosModules.server
+    inputs.srvos.nixosModules.mixins-systemd-boot
+    inputs.srvos.nixosModules.mixins-terminfo
+    inputs.srvos.nixosModules.mixins-trusted-nix-caches
+    inputs.disko.nixosModules.disko
+    inputs.nixos-facter-modules.nixosModules.facter
+    flake.modules.shared.default
+    flake.modules.nixos.default
+    ./disk-config.nix
+  ];
+
+  networking.hostName = "bubbi";
+
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+  facter.reportPath = ./facter.json;
+
+  users.users.root.initialHashedPassword = "$y$j9T$xA3OJK4WPx3Gu80.nTV6h/$DsBKf3OL11/d9bOAQmSVbgf2H2Ue4FAwhPLcatF0tX3";
+
+  boot.extraModprobeConfig = "options kvm_intel nested=1";
+  
+  # USB compatibility fixes for katla-frontpanel
+  boot.kernelParams = [
+    "usbcore.quirks=16c0:27dd:b"  # Disable USB autosuspend for katla device
+  ];
+  boot.initrd.availableKernelModules = [
+    # General
+    "ata_piix"
+    "sd_mod"
+    "sr_mod"
+    "usb_storage"
+    "sdhci_acpi"
+    "nvme" # NVMe support
+    "xhci_pci" # USB 3.0 support
+    "ahci" # SATA support
+    "usbhid" # USB HID support
+  ];
+
+  security.sudo.wheelNeedsPassword = false;
+  # USB device access for katla-frontpanel
+  services.udev.extraRules = ''
+    # Genki katla-frontpanel USB device (both product IDs) - NixOS style
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="27dd", MODE="0664", GROUP="users", TAG+="uaccess"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="20b1", MODE="0664", GROUP="users", TAG+="uaccess"
+  '';
+}
---
+++ hosts/bubbi/disk-config.nix
@@ -0,0 +1,37 @@
+# https://github.com/nix-community/disko/blob/master/example/hybrid.nix
+{
+  disko.devices = {
+    disk = {
+      main = {
+        device = "/dev/disk/by-id/ata-512GB_SSD_MQ18B75608583";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+            };
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
