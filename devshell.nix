f79aa7af535dabead729c9eeb115bc34c0b7cdab
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-09-04 15:09:20 +0000

    fix(devshell): remove unused deps

--- devshell.nix
+++ devshell.nix
@@ -6,14 +6,9 @@ pkgs.mkShellNoCC {
   packages =
     with pkgs;
     [
-      nixfmt-rfc-style
       git
       nixos-anywhere
       nixos-rebuild
-      age
-      age-plugin-yubikey
-      age-plugin-fido2-hmac
-      perSystem.self.deploy
       home-manager
     ]
     ++ lib.optionals stdenv.hostPlatform.isDarwin [
