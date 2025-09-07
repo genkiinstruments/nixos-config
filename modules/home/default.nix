7d46053db93280e074c5a51c22a0be28342d6512
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-09-07 14:29:19 +0200

    chore: nix fmt

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -61,13 +61,15 @@
       enable = true;
       package = pkgs.openssh;
       extraConfig = "SetEnv TERM=xterm-256color";
-      matchBlocks."*" = {
-        controlMaster = "auto";
-        controlPath = "/tmp/ssh-%u-%r@%h:%p";
-        controlPersist = "1800";
-        forwardAgent = true;
-        addKeysToAgent = "yes";
-        serverAliveInterval = 900;
+      matchBlocks = {
+        "*" = {
+          controlMaster = "auto";
+          controlPath = "/tmp/ssh-%u-%r@%h:%p";
+          controlPersist = "1800";
+          forwardAgent = true;
+          extraOptions.AddKeysToAgent = "yes";
+          serverAliveInterval = 900;
+        };
       };
     };
 
