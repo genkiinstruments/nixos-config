e0f3fcd566fd605940439a669e836c2cc0230502
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-11-04 10:23:28 +0000

    feat: update secretive/ssh-agent setup

--- modules/darwin/secretive.nix
+++ modules/darwin/secretive.nix
@@ -1,6 +1,7 @@
 _: {
   environment.etc."ssh/ssh_config.d/secretive.conf".text = ''
-    Host *
+    # GitHub hosts - use ControlMaster for reduced Touch ID prompts
+    Host github.com *.github.com
       # Secretive SSH agent socket
       IdentityAgent ~/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
 
@@ -8,17 +9,17 @@ _: {
       # After one authentication, reuse the connection for 10 minutes
       # This reduces the need for repeated Touch ID confirmations
       ControlMaster auto
-      ControlPath none
-      ControlPersist 30m
-
-      # Forward agent to remote hosts
-      ForwardAgent yes
+      ControlPath ~/.ssh/master-%r@%n:%p
+      ControlPersist 1h
 
       # Automatically add keys to the agent
       AddKeysToAgent yes
 
-      # Accept new host keys automatically (warn on changes)
-      StrictHostKeyChecking accept-new
+    # All other hosts - no ControlMaster for better interactive sessions
+    Host *
+      IdentityAgent ~/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
+      ForwardAgent yes
+      AddKeysToAgent yes
   '';
 
   homebrew = {
