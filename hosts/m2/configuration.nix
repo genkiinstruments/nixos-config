598da07c967b8cf51c4530151f694644e4567ae8
multivac61 <multivac61@users.noreply.github.com>
2025-11-11 12:57:16 +0000

    M2 bringup (#390)
    * feat: m2
    
    * feat(builders): add m2
    
    * chore: update flake.lock (#388)
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * chore: nix fmt
    
    ---------
    
    Co-authored-by: auto-merge-genki[bot] <201739797+auto-merge-genki[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ hosts/m2/configuration.nix
@@ -0,0 +1,88 @@
+{
+  inputs,
+  flake,
+  lib,
+  ...
+}:
+{
+  imports = [
+    inputs.srvos.nixosModules.server
+    inputs.srvos.nixosModules.mixins-terminfo
+    inputs.srvos.nixosModules.mixins-systemd-boot
+    inputs.nixos-facter-modules.nixosModules.facter
+    inputs.agenix.nixosModules.default
+    flake.modules.shared.default
+    flake.modules.nixos.default
+    flake.modules.nixos.ssh-serve
+    inputs.nixos-apple-silicon.nixosModules.apple-silicon-support
+  ];
+
+  networking.hostName = "m2";
+  system.stateVersion = "25.11"; # Did you read the comment?
+  facter.reportPath = ./facter.json;
+
+  # the issue is that logind allocates 25% of your system memory to /run rather than more by default, we need to increase that so that builds don't fail
+  services.logind.settings.Login.RuntimeDirectorySize = "50%";
+  boot.runSize = "50%";
+
+  hardware.asahi = {
+    enable = true;
+    extractPeripheralFirmware = true;
+    peripheralFirmwareDirectory = ./firmware;
+  };
+  hardware.graphics.enable32Bit = lib.mkForce false;
+
+  zramSwap = {
+    enable = true;
+    algorithm = "zstd";
+    # This refers to the uncompressed size, actual memory usage will be lower.
+    memoryPercent = 50;
+  };
+
+  boot = {
+    # https://rdx.overdevs.com/comments.html?url=https://www.reddit.com/r/AsahiLinux/comments/1gy0t86/psa_transitioning_from_zramswap_to_zswap/
+    kernelParams = [
+      "zswap.zpool=zsmalloc"
+      # Disable WiFi to prevent issues
+      "module_blacklist=brcmfmac"
+    ];
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = lib.mkForce false;
+    };
+    initrd.availableKernelModules = [
+      "sd_mod"
+      "sdhci_pci"
+      "usb_storage"
+      "usbhid"
+      "xhci_pci"
+    ];
+    initrd.kernelModules = [
+      "usbhid"
+      "dm-snapshot"
+    ];
+  };
+  # Disable wireless networking entirely
+  networking.wireless.enable = false;
+  networking.wireless.iwd.enable = false;
+  boot.extraModprobeConfig = ''blacklist brcmfmac'';
+
+  # Enable wired networking for built-in ethernet and USB-C adapter
+  networking.useDHCP = false;
+  networking.interfaces.enu1.useDHCP = true;
+  networking.interfaces.end0.useDHCP = true;
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/7468e770-e65a-418e-ab99-22daab5274b9";
+    fsType = "ext4";
+  };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/4510-16F0";
+    fsType = "vfat";
+    options = [
+      "fmask=0022"
+      "dmask=0022"
+    ];
+  };
+  swapDevices = [ ];
+}
