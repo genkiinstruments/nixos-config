2ea1a6c351a990a2f0afd88d6945d35e8430b091
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-11-20 16:44:38 +0000

    chore(m3): syntax

--- hosts/m3/users/olafur.nix
+++ hosts/m3/users/olafur.nix
@@ -7,9 +7,7 @@
 
   home.sessionVariables.MVIM_CONFIG_SOURCE = "/private/etc/nixos-config/home/nvim";
 
-  programs.git.settings = {
-    user.email = "olafur@genkiinstruments.com";
-    user.name = "multivac61";
-    github.user = "multivac61";
-  };
+  programs.git.settings.user.email = "olafur@genkiinstruments.com";
+  programs.git.settings.user.name = "multivac61";
+  programs.git.settings.github.user = "multivac61";
 }
