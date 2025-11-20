acd2fa4658060cf6261635d8deb664e93f4a922c
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-11-20 16:44:48 +0000

    feat(x): olafur git settings

--- hosts/x/users/olafur.nix
+++ hosts/x/users/olafur.nix
@@ -1,4 +1,8 @@
 { flake, ... }:
 {
   imports = [ flake.modules.home.default ];
+
+  programs.git.settings.user.email = "olafur@genkiinstruments.com";
+  programs.git.settings.user.name = "multivac61";
+  programs.git.settings.github.user = "multivac61";
 }
