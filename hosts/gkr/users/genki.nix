d9b58b98264369f1ae0b94d768cee827a4fea3cb
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-11-27 15:52:51 +0000

    chore(gkr): forgot git.settings

--- hosts/gkr/users/genki.nix
+++ hosts/gkr/users/genki.nix
@@ -1,10 +1,7 @@
 { flake, ... }:
 {
-  imports = [ flake.modules.home.default ];
-
-  programs.git.settings = {
-    user.email = "olafur@genkiinstruments.com";
-    user.name = "multivac61";
-    github.user = "multivac61";
-  };
+  imports = [
+    flake.modules.home.default
+    flake.modules.home.olafur
+  ];
 }
