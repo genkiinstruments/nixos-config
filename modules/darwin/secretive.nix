d48a9c69fc64edcc1b30b4fa1e5f61ebc55d3559
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-12-09 10:34:31 +0000

    fix(secretive): give nix daemon access to socket

--- modules/darwin/secretive.nix
+++ modules/darwin/secretive.nix
@@ -1,10 +1,22 @@
+{ config, ... }:
+let
+  user = config.system.primaryUser;
+  userSocketPath = "/Users/${user}/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh";
+  sharedSocketPath = "/var/run/secretive/agent.sock";
+in
 {
   environment.etc."ssh/ssh_config.d/secretive.conf".text = ''
     Host *
-      IdentityAgent ~/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
+      IdentityAgent ${sharedSocketPath}
       ControlMaster auto
       ControlPersist 10m
       ControlPath ~/.ssh/sockets/%r@%h:%p
+  '';
+
+  # Create symlink to Secretive socket accessible by nix-daemon
+  system.activationScripts.postActivation.text = ''
+    mkdir -p /var/run/secretive ~/.ssh/sockets
+    ln -sfn ${userSocketPath} ${sharedSocketPath}
   '';
 
   homebrew = {
