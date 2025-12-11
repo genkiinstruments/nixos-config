17c883df89830d0dfcca686b7eec8484566fda25
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-12-11 16:38:58 +0000

    feat(kroli): use zfs instead

--- hosts/kroli/disk-config.nix
+++ hosts/kroli/disk-config.nix
@@ -1,34 +1,74 @@
-# https://github.com/nix-community/disko/blob/master/example/hybrid.nix
 {
+  networking.hostId = "00000000"; # ZFS needs this set
+  services.zfs.autoScrub.enable = true;
+  boot.supportedFilesystems = [ "zfs" ];
+
   disko.devices = {
     disk = {
-      main = {
+      disk1 = {
         device = "/dev/disk/by-id/nvme-nvme.1e4b-4d513138423335353036323332-353132474220535344-00000001";
         type = "disk";
         content = {
           type = "gpt";
           partitions = {
-            boot = {
-              size = "1M";
-              type = "EF02"; # for grub MBR
-            };
             ESP = {
-              size = "512M";
               type = "EF00";
+              size = "2G";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
               };
             };
-            root = {
+            zfs = {
               size = "100%";
               content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
+                type = "zfs";
+                pool = "rpool";
               };
             };
+          };
+        };
+      };
+    };
+    zpool = {
+      rpool = {
+        type = "zpool";
+        rootFsOptions = {
+          acltype = "posixacl";
+          compression = "zstd";
+          dnodesize = "auto";
+          normalization = "formD";
+          relatime = "on";
+          xattr = "sa";
+        };
+        options = {
+          ashift = "12";
+          autotrim = "on";
+        };
+
+        datasets = {
+          "root" = {
+            type = "zfs_fs";
+            options = {
+              mountpoint = "legacy";
+            };
+            mountpoint = "/";
+          };
+          "nix" = {
+            type = "zfs_fs";
+            options.mountpoint = "legacy";
+            mountpoint = "/nix";
+          };
+          "var" = {
+            type = "zfs_fs";
+            options.mountpoint = "legacy";
+            mountpoint = "/var";
+          };
+          "home" = {
+            type = "zfs_fs";
+            mountpoint = "/home";
+            options.mountpoint = "legacy";
           };
         };
       };
