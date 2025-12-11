ca867420203a20dedb6bb62e7472449859589bdf
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-12-11 15:27:41 +0000

    feat(kroli): remove olafur user

--- hosts/kroli/configuration.nix
+++ hosts/kroli/configuration.nix
@@ -12,10 +12,8 @@
     inputs.disko.nixosModules.disko
     inputs.nixos-facter-modules.nixosModules.facter
     flake.modules.shared.default
-    flake.modules.shared.home-manager
     flake.modules.nixos.default
     flake.modules.nixos.comin
-    flake.modules.nixos.olafur
     flake.modules.nixos.katla-udev
     flake.modules.nixos.yggdrasil
     ./disk-config.nix
@@ -23,7 +21,4 @@
 
   system.stateVersion = "23.05";
   facter.reportPath = ./facter.json;
-
-  # We are using zfs: https://github.com/atuinsh/atuin/issues/952#issuecomment-2199964530
-  home-manager.users.olafur.programs.atuin.daemon.enable = true;
 }
