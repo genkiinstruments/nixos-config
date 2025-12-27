6934ddc2a31e85df19c837a6bb2315de1fb05b19
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-12-27 14:02:30 +0000

    feat: add devshell that points NVIM_APPNAME to nvim-dev so I can get the best of both worlds, nix run, and instant reload of lua config

--- devshell.nix
+++ devshell.nix
@@ -1,11 +1,27 @@
-{ pkgs, perSystem }:
+{
+  pkgs,
+  perSystem,
+  ...
+}:
+let
+  nvim = perSystem.self.nvim;
+in
 pkgs.mkShellNoCC {
-  packages = with pkgs; [
-    git
-    nixos-anywhere
-    nh
+  packages = [
+    pkgs.git
+    pkgs.nixos-anywhere
+    pkgs.nh
     perSystem.self.all
     perSystem.self.ni
-  ];
-  env.EDITOR = "nvim";
+    nvim.neovim-nightly
+  ] ++ nvim.tools;
+
+  env = {
+    EDITOR = "nvim";
+    NVIM_APPNAME = "nvim-dev";
+  };
+
+  shellHook = ''
+    ln -sfn "$(realpath ./home/nvim)" ~/.config/nvim-dev
+  '';
 }
--- packages/nvim.nix
+++ packages/nvim.nix
@@ -13,36 +13,47 @@ let
   };
 
   configDir = "${flake}/home/nvim";
-in
-pkgs.wrapNeovimUnstable neovim-nightly {
-  luaRcContent = ''
-    vim.opt.rtp:prepend("${configDir}")
-    dofile("${configDir}/init.lua")
-  '';
-  vimAlias = true;
-  viAlias = false;
+
+  tools = [
+    pkgs.git
+    pkgs.ripgrep
+    pkgs.fd
+    pkgs.fzf
+    pkgs.nodejs
+    pkgs.tree-sitter
+    pkgs.nixfmt-rfc-style
+    pkgs.nil
+    pkgs.rust-analyzer
+    pkgs.gopls
+    pkgs.lua-language-server
+    pkgs.nodePackages.typescript-language-server
+    pkgs.nodePackages.vscode-langservers-extracted
+    pkgs.pyright
+    pkgs.ruff
+    pkgs.stylua
+    perSystem.expert.default
+  ];
+
   wrapperArgs = [
     "--prefix"
     "PATH"
     ":"
-    "${pkgs.lib.makeBinPath [
-      pkgs.git
-      pkgs.ripgrep
-      pkgs.fd
-      pkgs.fzf
-      pkgs.nodejs
-      pkgs.tree-sitter
-      pkgs.nixfmt-rfc-style
-      pkgs.nil
-      pkgs.rust-analyzer
-      pkgs.gopls
-      pkgs.lua-language-server
-      pkgs.nodePackages.typescript-language-server
-      pkgs.nodePackages.vscode-langservers-extracted
-      pkgs.pyright
-      pkgs.ruff
-      pkgs.stylua
-      perSystem.expert.default
-    ]}"
+    "${pkgs.lib.makeBinPath tools}"
   ];
+
+  wrapped = pkgs.wrapNeovimUnstable neovim-nightly {
+    luaRcContent = ''
+      vim.opt.rtp:prepend("${configDir}")
+      dofile("${configDir}/init.lua")
+    '';
+    vimAlias = true;
+    viAlias = false;
+    inherit wrapperArgs;
+  };
+in
+wrapped.overrideAttrs {
+  passthru = {
+    # Expose for devshell to create config-less version
+    inherit neovim-nightly tools wrapperArgs;
+  };
 }
