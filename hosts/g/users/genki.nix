71e33a2e9478a7762a7f9d6aa175562045da5366
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-12-29 14:41:53 +0000

    feat(g): add olafur profile to genki in d

--- hosts/g/users/genki.nix
+++ hosts/g/users/genki.nix
@@ -3,11 +3,8 @@
   ...
 }:
 {
-  imports = [ flake.modules.home.default ];
-
-  programs.git.settings = {
-    user.email = "olafur@genkiinstruments.com";
-    user.name = "multivac61";
-    github.user = "multivac61";
-  };
+  imports = [
+    flake.modules.home.default
+    flake.modules.home.olafur
+  ];
 }
