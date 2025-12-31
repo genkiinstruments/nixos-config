7db825460490e2ea68171ade9e177244c38a3064
multivac61 <olafur@genkiinstruments.com>
2025-12-31 16:39:35 +0000

    feat: replace catppuccin-nix with stylix

--- hosts/g/users/genki.nix
+++ hosts/g/users/genki.nix
@@ -9,23 +9,5 @@
     flake.modules.home.niri
   ];
 
-  catppuccin.gtk.icon.enable = true;
-
-  # GTK dark theme (catppuccin gtk was archived, using Adwaita dark)
-  gtk = {
-    enable = true;
-    theme = {
-      name = "Adwaita-dark";
-    };
-  };
-
-  # Prefer dark mode for all apps
-  dconf.settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
-
-  # Qt dark theme
-  qt = {
-    enable = true;
-    platformTheme.name = "kvantum";
-    style.name = "kvantum";
-  };
+  # Stylix handles GTK/Qt theming automatically
 }
