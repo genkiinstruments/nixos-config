a30e9a74e62f3fddc7718f9df8abc799837a8c62
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2026-01-02 10:44:38 +0000

    fix: caddy sha

--- hosts/gdrn/configuration.nix
+++ hosts/gdrn/configuration.nix
@@ -64,7 +64,7 @@
 
     package = pkgs.caddy.withPlugins {
       plugins = [ "github.com/caddy-dns/cloudflare@v0.2.2" ];
-      hash = "sha256-ea8PC/+SlPRdEVVF/I3c1CBprlVp1nrumKM5cMwJJ3U=";
+      hash = "sha256-dnhEjopeA0UiI+XVYHYpsjcEI6Y1Hacbi28hVKYQURg=";
     };
 
     virtualHosts."uptime-kuma.genki.is".extraConfig = ''
