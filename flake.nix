752d86b367e983bfb0ef367646bffe1b449cb0eb
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2026-01-13 07:10:48 +0000

    chore(flake): make inputs shallow

--- flake.nix
+++ flake.nix
@@ -5,60 +5,60 @@
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable&shallow=1";
     # nixpkgs.url = "github:NixOS/nixpkgs?ref=master&shallow=1";
 
-    srvos.url = "github:nix-community/srvos";
+    srvos.url = "github:nix-community/srvos?shallow=1";
     srvos.inputs.nixpkgs.follows = "nixpkgs";
 
-    agenix.url = "github:ryantm/agenix";
+    agenix.url = "github:ryantm/agenix?shallow=1";
     agenix.inputs.nixpkgs.follows = "nixpkgs";
     agenix.inputs.darwin.follows = "nix-darwin";
     agenix.inputs.home-manager.follows = "home-manager";
 
-    blueprint.url = "github:numtide/blueprint";
+    blueprint.url = "github:numtide/blueprint?shallow=1";
     blueprint.inputs.nixpkgs.follows = "nixpkgs";
 
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager?shallow=1";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    nix-darwin.url = "github:LnL7/nix-darwin/master";
+    nix-darwin.url = "github:LnL7/nix-darwin/master?shallow=1";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
-    disko.url = "github:nix-community/disko";
+    disko.url = "github:nix-community/disko?shallow=1";
     disko.inputs.nixpkgs.follows = "nixpkgs";
 
-    stylix.url = "github:nix-community/stylix";
+    stylix.url = "github:nix-community/stylix?shallow=1";
     stylix.inputs.nixpkgs.follows = "nixpkgs";
 
-    base16-schemes.url = "github:tinted-theming/schemes";
+    base16-schemes.url = "github:tinted-theming/schemes?shallow=1";
     base16-schemes.flake = false;
 
-    stripe-webshippy-sync.url = "github:genkiinstruments/stripe-webshippy-sync";
+    stripe-webshippy-sync.url = "github:genkiinstruments/stripe-webshippy-sync?shallow=1";
     stripe-webshippy-sync.inputs.nixpkgs.follows = "nixpkgs";
 
-    secrets.url = "github:genkiinstruments/nix-secrets";
+    secrets.url = "github:genkiinstruments/nix-secrets?shallow=1";
     secrets.flake = false;
 
-    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.url = "github:numtide/treefmt-nix?shallow=1";
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
 
-    nixos-facter-modules.url = "github:nix-community/nixos-facter-modules";
+    nixos-facter-modules.url = "github:nix-community/nixos-facter-modules?shallow=1";
 
-    buildbot-nix.url = "github:nix-community/buildbot-nix";
+    buildbot-nix.url = "github:nix-community/buildbot-nix?shallow=1";
     buildbot-nix.inputs.nixpkgs.follows = "nixpkgs";
 
-    comin.url = "github:nlewo/comin";
+    comin.url = "github:nlewo/comin?shallow=1";
     comin.inputs.nixpkgs.follows = "nixpkgs";
 
     # Apple Silicon support for m2
-    nixos-apple-silicon.url = "github:tpwrules/nixos-apple-silicon";
+    nixos-apple-silicon.url = "github:tpwrules/nixos-apple-silicon?shallow=1";
     nixos-apple-silicon.inputs.nixpkgs.follows = "nixpkgs";
 
-    neovim-nightly.url = "github:neovim/neovim";
+    neovim-nightly.url = "github:neovim/neovim?shallow=1";
     neovim-nightly.flake = false;
 
-    yazi-flavors.url = "github:yazi-rs/flavors";
+    yazi-flavors.url = "github:yazi-rs/flavors?shallow=1";
     yazi-flavors.flake = false;
 
-    expert.url = "github:elixir-lang/expert";
+    expert.url = "github:elixir-lang/expert?shallow=1";
     expert.inputs.nixpkgs.follows = "nixpkgs";
   };
 
