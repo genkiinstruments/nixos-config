5378d09b099c94a3aef32c128fa5365313b2957a
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2026-01-16 06:44:31 +0000

    chore(secretive): simplify module and remove symlinks (deleted at reboot/updates on macos)

--- modules/darwin/secretive.nix
+++ modules/darwin/secretive.nix
@@ -1,24 +1,17 @@
 { config, ... }:
 let
   user = config.system.primaryUser;
-  userSocketPath = "/Users/${user}/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh";
-  sharedSocketPath = "/var/run/secretive/agent.sock";
 in
 {
   environment.etc."ssh/ssh_config.d/secretive.conf".text = ''
     Host *
-      IdentityAgent ${sharedSocketPath}
+      IdentityAgent ~/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
       ControlMaster auto
       ControlPersist 10m
       ControlPath ~/.ssh/sockets/%r@%h:%p
   '';
 
-  # Create symlink to Secretive socket accessible by nix-daemon
   system.activationScripts.postActivation.text = ''
-    mkdir -p /var/run/secretive
-    ln -sfn ${userSocketPath} ${sharedSocketPath}
-
-    # Create SSH control socket directory with correct ownership
     install -d -o ${user} -m 700 /Users/${user}/.ssh/sockets
   '';
 
